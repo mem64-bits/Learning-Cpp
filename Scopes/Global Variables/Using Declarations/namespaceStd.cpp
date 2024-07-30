7df03eb2843ec2e5478f58adbec38660e669f987
shade64 <toryokodogbe270@gmail.com>
2024-07-30 22:29:42 +0000

    30 Jul 2024, 23:29

---
+++ Scopes/Global Variables/Using Declarations/namespaceStd.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(){
+    /*allows us to use all functions from
+    std namespace without std::*/
+    using namespace std;
+    cout<<"Hello World";
+    return 0;
+}
