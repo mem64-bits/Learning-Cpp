62e587e28872bb6550b2614a888d62419113adde
shade64 <toryokodogbe270@gmail.com>
2024-07-30 14:03:54 +0100

    commit

--- Scopes/Global Variables/internalLinking.cpp
+++ Scopes/Global Variables/internalLinking.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
-//All variables are externally linked by default
+/*An identifier with internal linkage can be seen 
+and used within a single translation unit, but it 
+is not accessible from other translation units*/
+
+// All variables are externally linked by default
 // the static keyword makes variables internally linked 
 static int g_x{5};
 
-const int g_y{}
+// consts are internally linked by default
+const int g_y{1}  
 
+// constexprs are internally linked by default
+constexpr int g_z{2};
 int main(){
 
     return 0;
