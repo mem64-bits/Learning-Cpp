9cd75aae278da1264e947f4f3bc1503ccfb8a54f
shade64 <toryokodogbe270@gmail.com>
2024-07-30 11:38:52 +0100

    commit

---
+++ Scopes/Global Variables/internalLinking.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//All variables are externally linked by default
+// the static keyword makes variables internally linked 
+static int g_x{5};
+
+const int g_y{}
+
+int main(){
+
+    return 0;
+}
