5be0d97c41d8e1461a359987ecc33daf420a1e3d
shade64 <toryokodogbe270@gmail.com>
2024-07-30 16:10:25 +0000

    30 Jul 2024, 17:10

---
+++ Scopes/Global Variables/External Linkage/extern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*Global variables with external linkage are sometimes called 
+external variables. To make a global variable external we can 
+use the extern keyword to do so:*/
+
+// non-constant globals are external by default
+int g_x { 2 }; 
+
+// const globals can be defined as extern, making them external
+extern const int g_y { 3 }; // can be used in other files
+
+// Don't use extern on constexpt
+extern constexpr int g_z { 3 };
+
+int main(){
+
+    return 0;
+}
