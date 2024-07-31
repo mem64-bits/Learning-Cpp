f0a66214964e353fef143dc93d6dad943e8c6d5b
shade64 <toryokodogbe270@gmail.com>
2024-07-31 21:43:13 +0000

    31 Jul 2024, 22:43

---
+++ Control Statements/Switch Statements/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+
+
+/* The downside to using return to stop the execution of 
+switches is that it exits put of the whole function 
+instead of just exiting the switch statement*/
+    
+
+void printDigitName(int x)
+{
+    switch (x) 
---
+++ Control Statements/Switch Statements/usingBreaksinSwitches.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+/* The downside to using return to stop the execution of 
+switches is that it exits put of the whole function 
+instead of just exiting the switch statement*/
+
+
+int main(){
+    #include <iostream>
+
+void printDigitName(int x)
+{
+    switch (x) // x evaluates to 3
+    {
+    case 1:
+        std::cout << "One";
+        break;
+    case 2:
+        std::cout << "Two";
+        break;
+    case 3:
+        std::cout << "Three"; // execution starts here
+        break; // jump to the end of the switch block
+    default:
+        std::cout << "Unknown";
+        break;
+    }
+
+    // execution continues here
+    std::cout << "Didn't exit the function after the switch statement !\n";
+}
+
+int main()
+{
+    printDigitName(3);
+    std::cout << '\n';
+
+    return 0;
+}
