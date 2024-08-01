43e9579df5d0c6bba236778ece1b2e25f5fa523c
shade64 <toryokodogbe270@gmail.com>
2024-08-01 21:24:50 +0000

    1 Aug 2024, 22:24

--- Control Statements/While Statements/counter.cpp
+++ Control Statements/While Statements/counter.cpp
@@ -3,10 +3,15 @@
 
 void counter(int printCount){
     int num{1};
+
+    if(num > 0)
+        std::cout<<"Function parameter printCount must be positive";
+    
     while(printCount>0){
         std::cout<<num<<'\n';
         ++num;
-      --printCount;
+        --printCount;
+    
     }
 }
 
