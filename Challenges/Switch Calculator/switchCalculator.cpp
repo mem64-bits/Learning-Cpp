53d709c8d76a50a33f944ad72c62ff88c7ed3c1c
shade64 <toryokodogbe270@gmail.com>
2024-08-01 18:25:29 +0000

    1 Aug 2024, 19:25

---
+++ Challenges/Switch Calculator/switchCalculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+int calculate(int x, char oper, int y ){
+
+    switch(oper)
+    {
+        case '+':
+            return x+y;
+        case '-':
+            return x-y;
+        case 'x':
+            return x*y;
+        case '%':
+            return x%y;
+        case '/':
+            return x/y;
+        default:
+            std::cout<<"Error: Invalid Operator";
+            return;
+
+
+    }
+
+}
+
+int main(){
+    std::cout<<calculate(5,'+',5)<<'\n';
+    std::cout<<calculate(7,'-',4)<<'\n';
+    std::cout<<calculate(10,'x',3)<<'\n';
+    std::cout<<calculate(10,'/',2)<<'\n';
+    std::cout<<calculate(10,'b',10)<<'\n';
+    return 0;
+}
