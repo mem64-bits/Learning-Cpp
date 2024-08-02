e4e7f6cc4b652f453f54ab77067636c37188c8b3
shade64 <toryokodogbe270@gmail.com>
2024-08-02 21:01:15 +0000

    2 Aug 2024, 22:01

--- Control Statements/While Statements/nestedLoopsQs.cpp
+++ Control Statements/While Statements/nestedLoopsQs.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 
 int main(){
-      // outer loops between 1 and 5
-    int outer{ 5 };
-    while (outer <= 5)
-    {
-        // For each iteration of the outer loop, the code in the body of the loop executes once
-
-        // inner loops between 1 and outer
-        int inner{ 5};
-        while (inner <= outer)
-        {
-            std::cout << inner << ' ';
-            --inner;
-        }
-
-        // print a newline at the end of each row
-        std::cout << '\n';
-        --outer;
+    int outer{5};
+    while (outer>0){
+        int inner{5}
+        while inner
     }
+      
     return 0;
 }
