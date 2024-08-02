580613cbe6ae42c8f965a5d6d5c97b6ab49c40fa
shade64 <toryokodogbe270@gmail.com>
2024-08-02 17:45:16 +0000

    2 Aug 2024, 18:45

--- Control Statements/While Statements/NestedLoops.cpp
+++ Control Statements/While Statements/NestedLoops.cpp
@@ -3,7 +3,7 @@
 int main(){
       // outer loops between 1 and 5
     int outer{ 1 };
-    while (outer <= 6)
+    while (outer <= 5)
     {
         // For each iteration of the outer loop, the code in the body of the loop executes once
 
