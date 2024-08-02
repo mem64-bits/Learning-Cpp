c41ba75872bc1c46564eedfa8b3ab071ab764f95
shade64 <toryokodogbe270@gmail.com>
2024-08-02 18:09:26 +0000

    2 Aug 2024, 19:09

--- Challenges/Nested Loops/nestedLoops.cpp
+++ Challenges/Nested Loops/nestedLoops.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
-
-int main(){
-    char letter{'a'};
+void charCodePrinter(){
+char letter{'a'};
     while(letter <= 'z'){
         int chr_code{static_cast<int>(letter)};
         std::cout<<letter<<" ASCII Code: "<<chr_code<<'\n';
         ++letter;
-    }
+}
+}
+
+int main(){
+    charCodePrinter();
     return 0;
 }  
