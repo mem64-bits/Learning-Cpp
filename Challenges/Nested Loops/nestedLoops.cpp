08328c8c1d68d06e87b1722e5aae54a53285b206
shade64 <toryokodogbe270@gmail.com>
2024-08-02 17:59:21 +0000

    2 Aug 2024, 18:59

--- Challenges/Nested Loops/nestedLoops.cpp
+++ Challenges/Nested Loops/nestedLoops.cpp
@@ -3,9 +3,10 @@
 
 int main(){
     char letter{96};
-    while(letter >=96 && letter <= 122){
+    while(letter >=97 && letter <= 122){
         int chr_code{static_cast<int>(letter)};
         std::cout<<letter<<" ASCII Code: "<<chr_code<<'\n';
+        ++letter;
     }
     return 0;
 }  
