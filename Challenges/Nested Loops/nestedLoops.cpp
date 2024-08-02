3fab8d6d103f1b4d04ed1d54d57fc72c60124c3e
shade64 <toryokodogbe270@gmail.com>
2024-08-02 18:18:06 +0000

    2 Aug 2024, 19:18

--- Challenges/Nested Loops/nestedLoops.cpp
+++ Challenges/Nested Loops/nestedLoops.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-void charCodePrinter(){
-char letter{'a'};
+
+int main(){
+    char letter{'a'};
     while(letter <= 'z'){
         int chr_code{static_cast<int>(letter)};
         std::cout<<letter<<" ASCII Code: "<<chr_code<<'\n';
         ++letter;
-}
-}
-
-int main(){
-    charCodePrinter();
+    }
     return 0;
 }  
---
+++ Control Statements/While Statements/nestedLoopsQs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+      // outer loops between 1 and 5
+    int outer{ 5 };
+    while (outer <= 5)
+    {
+        // For each iteration of the outer loop, the code in the body of the loop executes once
+
+        // inner loops between 1 and outer
+        int inner{ 5};
+        while (inner <= outer)
+        {
+            std::cout << inner << ' ';
+            --inner;
+        }
+
+        // print a newline at the end of each row
+        std::cout << '\n';
+        --outer;
+    }
+    return 0;
+}
