8f6fc9998649a3e139b05c1ee8ae7848acb361da
shade64 <toryokodogbe270@gmail.com>
2024-08-03 12:51:56 +0000

    3 Aug 2024, 13:51

--- Challenges/Nested Loops/ForLoops/fizzbuzz.cpp
+++ Challenges/Nested Loops/ForLoops/fizzbuzz.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <chrono>
 
 
 void fizz_buzz(int end_range){
-    for (int i{0}; i <= end_range; ++i){
+    for (int i{1}; i <= end_range; ++i){
 
-        if(i%3 == 0  && i%5 == 0){
+        if((i%3 == 0 ) && (i%5 == 0) && (i%7 == 0)){
+            std::cout<<"fizzbuzzpop\n";
+        }
+
+        else if(i%3 == 0  && i%5 == 0){
             std::cout<<"fizzbuzz\n";
         }
 
@@ -16,6 +21,10 @@ void fizz_buzz(int end_range){
             std::cout<<"buzz\n";
         }
 
+        else if (i % 7 == 0){
+            std::cout<<"pop\n";
+        }
+
 
         else {
             std::cout<<i<<'\n';
@@ -23,8 +32,46 @@ void fizz_buzz(int end_range){
     }   
 }
 
+void fizz_buzz_v2(int end_range){
+    for(int i{0}; i <= end_range; ++i){
+        bool divisible{false};
 
+        if (i % 3 == 0){
+            std::cout<<"fizz\n";
+            divisible = true;
+        }
+
+        if (i%5 == 0){
+            std::cout<<"buzz\n";
+            divisible = true;
+        }
+
+         if (i%7 == 0){
+            std::cout<<"pop\n";
+            divisible = true;
+        }
+
+        if (!divisible){
+            std::cout<<i<<'\n';
+        }
+            
+        }
+
+    }
+    
+        
 int main(){
+    using namespace std::chrono;
+    auto start = high_resolution_clock::now();
     fizz_buzz(15);
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    
+    std::cout << "Time taken by function: "
+         << duration.count() << " microseconds" << std::endl;
+ 
+    std::cout<<'\n';
+    fizz_buzz_v2(150);
     return 0;
 }
