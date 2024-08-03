b930a4b58a04e078f643d919c026bee92132d58f
shade64 <toryokodogbe270@gmail.com>
2024-08-03 11:35:10 +0000

    3 Aug 2024, 12:35

---
+++ Challenges/Nested Loops/ForLoops/fizzbuzz.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+void fizz_buzz(int end_range){
+    for (int i{0}; i <= end_range; ++i){
+        if (i % 3 ==0){
+            std::cout<<"fizz\n";
+        }
+
+        else if(i % 5 == 0){
+            std::cout<<"buzz\n";
+        }
+
+        else {
+            std::cout<<i<<'\n';
+        }
+    }
+}
