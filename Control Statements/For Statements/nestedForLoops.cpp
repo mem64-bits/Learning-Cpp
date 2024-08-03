b1236e941a9ae60297c46c6da202626160b1994e
shade64 <toryokodogbe270@gmail.com>
2024-08-03 09:55:45 +0000

    3 Aug 2024, 10:55

--- Control Statements/For Statements/nestedForLoops.cpp
+++ Control Statements/For Statements/nestedForLoops.cpp
@@ -4,9 +4,8 @@
 
 int main(){
     for(int i{1}; i <=10; ++i){
-        std::cout<<i;
 
-        for (char chr{'a'}; chr < 'j'; ++chr){
+        for (char chr{'a'}; chr <= 'j'; ++chr){
             std::cout<< '('<<chr<< ')' <<'\n';
         }
 
