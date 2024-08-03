1df5e781c813eaf3a9ff1e43a4bae0bfffac947a
shade64 <toryokodogbe270@gmail.com>
2024-08-03 11:15:35 +0000

    3 Aug 2024, 12:15

---
+++ Challenges/Nested Loops/ForLoops/sumTo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int sumTo(int value){
+    int result{};
+    for (value; value > 0;){
+        result = value + (value-1);
+    return result;
+    }
+
+
+}
+ 
+int main(){
+    std::cout<<sumTo(5);
+    return 0;
+}
