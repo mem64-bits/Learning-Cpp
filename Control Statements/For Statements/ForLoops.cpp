7cbb47357105e2b46bf88966fc490ae2efaac737
shade64 <toryokodogbe270@gmail.com>
2024-08-03 09:02:40 +0000

    3 Aug 2024, 10:02

--- Control Statements/For Statements/ForLoops.cpp
+++ Control Statements/For Statements/ForLoops.cpp
@@ -9,11 +9,10 @@ void repeatMsg(std::string_view msg, int echo, char end){
     
 }
 int main(){
-    #if 0
-    for (int i{1}; i < 10; ++i){
+    for (int i{1}; i <= 10; ++i){
         std::cout<<"Hello World! \n";
     }
-    #endif
+    
 
     repeatMsg("This is a repeated message", 5, '\n');
     return 0;
