0c7978bc7d5c9adb83df57fd6c1a7c969c4aa99f
shade64 <toryokodogbe270@gmail.com>
2024-08-07 10:18:25 +0000

    7 Aug 2024, 11:18

--- Functions/OverLoading/overloadingFunctions.cpp
+++ Functions/OverLoading/overloadingFunctions.cpp
@@ -12,8 +12,17 @@ double add(double x, double y){
     return x+y;
 }
 
+// we can overload by adding more parameters to a function
+int add(int x, int y, int z){
+    return x+y+z;
+}
+
 int main(){
 std::cout<<add(7,5)<<'\n';
-std::cout<<add(12.5,12.5);
+
+// Overload resolution
+std::cout<<add(12.5,7.5)<<'\n';
+std::cout<<add(13,7,10);
+
 return 0;
 }
