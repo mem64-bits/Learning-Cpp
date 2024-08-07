c9a4cda4fd421eac175e30cc59597e2135f3426f
shade64 <toryokodogbe270@gmail.com>
2024-08-07 18:15:03 +0000

    7 Aug 2024, 19:15

--- Functions/Function Templates/nonTypeTemplatePara.cpp
+++ Functions/Function Templates/nonTypeTemplatePara.cpp
@@ -11,9 +11,18 @@ void print()
     std::cout << N << '\n'; // use value of N here
 }
 
+// C ++ 17 we can use auto to automatically determine 
+// template parameters
+
+template <auto N>
+void print_v2(){
+    std::cout<<N<<'\n';
+}
+
 int main()
 {
     print<5>(); // 5 is our non-type template argument
+    print_v2<1.95>();
 
     return 0;
 }
