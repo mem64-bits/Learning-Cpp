09ba7b7fa38b3d72a625ff71a581ef56a462eedc
shade64 <toryokodogbe270@gmail.com>
2024-08-07 19:43:03 +0000

    7 Aug 2024, 20:43

---
+++ Challenges/Templates/mult.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <iostream>
+
+// write your mult function template here
+template <typename T>
+T mult( T x, int y){
+    mult (char x, char y) = delete;
+    return x * y;
+}
+
+
+int main()
+{
+	std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+
+	return 0;
+}
