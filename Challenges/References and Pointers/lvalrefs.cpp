bee17efb3b15e29c79143cfaff3dee16df98cda0
shade64 <toryokodogbe270@gmail.com>
2024-08-07 21:49:52 +0000

    7 Aug 2024, 22:49

--- Challenges/References and Pointers/lvalrefs.cpp
+++ Challenges/References and Pointers/lvalrefs.cpp
@@ -14,7 +14,19 @@ that can be used as an alias to variable x*/
 int main(){
 int& ref {x};
 
-std::cout<<x<<'\n';
-std::cout<<ref<<'\n';
+std::cout<<x<<'\n';// prints x
+std::cout<<ref<<'\n';// prints ref, is the same value as x
+std::cout<<'\n';
+
+x = 6; // changes x to 6
+std::cout<<x<<'\n'; //prints x
+std::cout<<ref<<'\n'; // ref also changes
+std::cout<<'\n';
+
+ref = 7;// changing the reference also changes x
+std::cout<<x<<'\n'; //prints x
+std::cout<<ref<<'\n'; // ref also changes
+std::cout<<'\n';
+
 return 0;
 }
