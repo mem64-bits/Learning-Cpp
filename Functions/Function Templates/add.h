8ce2b22da51154dfc8205b558caecf1c6a74c24e
shade64 <toryokodogbe270@gmail.com>
2024-08-07 16:11:04 +0000

    7 Aug 2024, 17:11

---
+++ Functions/Function Templates/add.h
@@ -0,0 +1,13 @@
+#ifndef T_ADD_H
+#define T_ADD_H
+
+
+namespace Math{
+
+template < typename T>
+T add(T x, T y){
+    return x+y;
+}
+
+}
+#endif
---
+++ Functions/Function Templates/templatesInHeaders.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "add.h"
+
+int main(){
+    std::cout<<Math::add(5,6);
+    return 0;
+}
