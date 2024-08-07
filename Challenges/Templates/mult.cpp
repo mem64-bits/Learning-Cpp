e30314068324b930c12669e778cc657a4e493df2
shade64 <toryokodogbe270@gmail.com>
2024-08-07 21:10:09 +0000

    7 Aug 2024, 22:10

--- Challenges/Templates/mult.cpp
+++ Challenges/Templates/mult.cpp
@@ -8,11 +8,26 @@ T mult( T x, int y){
     return x * y;
 }
 
+template <typename N>
+N add(N x, N y){
+    return x + y;
+}
 
+template <typename T, typename U>
+
+ auto sub( T x, U y){
+    return x - y;
+}
 int main()
 {
+
+	std::cout << add(2, 3) << '\n';
+	std::cout << add(1.2, 3.4) << '\n';
 	std::cout << mult(2, 3) << '\n';
 	std::cout << mult(1.2, 3) << '\n';
+    std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
 
 	return 0;
 }
