6a317b13f8ed72ccec9563451ef7bc02ac313fdd
shade64 <toryokodogbe270@gmail.com>
2024-08-07 10:10:22 +0000

    7 Aug 2024, 11:10

--- Functions/OverLoading/overloadingFunctions.cpp
+++ Functions/OverLoading/overloadingFunctions.cpp
@@ -3,6 +3,10 @@
 int add(int x, int y ){
     return x+y;
 }
+/*Function overloading allows us to create 
+multiple functions with the same name,
+ so long as each identically named 
+function has different parameter types */
 
 double add(double x, int y){
     return x+y;
