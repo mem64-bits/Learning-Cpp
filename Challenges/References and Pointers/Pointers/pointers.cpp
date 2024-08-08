58320aee63cfbf4d81f08e7e86b9476b3822d1a5
shade64 <toryokodogbe270@gmail.com>
2024-08-08 21:01:27 +0000

    8 Aug 2024, 22:01

--- Challenges/References and Pointers/Pointers/pointers.cpp
+++ Challenges/References and Pointers/Pointers/pointers.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
+// & is the memory address operator
+/* 
+* is the dereference variable 
+gets the value at a memory address*
+*/
 
 int main(){
     int x{};
+
+    // reference to x 
     int& y{x};
+
+    // prints out the memory address of x 
     std::cout<<&x<<'\n';
+
+    // reference shares memory address
     std::cout<<&y<<'\n';
+
+    std::cout<<*(&x);
 
     return 0;
 }
