e4c40a9f1dd451bf2647b06c7ae0512b1ce32e8f
shade64 <toryokodogbe270@gmail.com>
2024-08-08 21:04:09 +0000

    8 Aug 2024, 22:04

--- Challenges/References and Pointers/Pointers/pointers.cpp
+++ Challenges/References and Pointers/Pointers/pointers.cpp
@@ -7,17 +7,21 @@ gets the value at a memory address*
 */
 
 int main(){
-    int x{};
+    int x{5};
 
-    // reference to x 
+// reference to x 
     int& y{x};
+    
+//prints x 
+std::cout<<x<<'\n';
 
-    // prints out the memory address of x 
+// prints out the memory address of x 
     std::cout<<&x<<'\n';
 
-    // reference shares memory address
+// reference shares memory address as x
     std::cout<<&y<<'\n';
 
+// Prints the value stored at the mem address x
     std::cout<<*(&x);
 
     return 0;
