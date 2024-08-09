d8fe6383e150f4c40daae2b05946794ad808e2f1
shade64 <toryokodogbe270@gmail.com>
2024-08-09 16:40:11 +0000

    9 Aug 2024, 17:40

--- References and Pointers/Pointers/sizeOfPointers.cpp
+++ References and Pointers/Pointers/sizeOfPointers.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int main(){
-    char chPtr{};
-    int iPtr{};
-    long double ldPtr{};
+int main(){ // assume computer is 32-bit
+    char chPtr{}; // char is 1
+    int iPtr{};// int is usually 4
+    long double ldPtr{};// long doubles are 8 or 12 bytes
 
-    std::cout<<sizeof(chPtr)<<'\n';
+    std::cout<<sizeof(chPtr)<<'\n'; 
     std::cout<<sizeof(iPtr)<<'\n';
     std::cout<<sizeof(ldPtr)<<'\n';
     return 0;
