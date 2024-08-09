ac86ec4ddc3b40ecff20f73060711cfc263414d3
shade64 <toryokodogbe270@gmail.com>
2024-08-09 16:08:10 +0000

    9 Aug 2024, 17:08

--- References and Pointers/Pointers/assigningWithPointers.cpp
+++ References and Pointers/Pointers/assigningWithPointers.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 
 int main(){
-    int x{5};
-    int* ptr{&x};
+    int x{5};  
+    int* ptr{&x}; // creates pointer to 
 
     std::cout<<*ptr<<'\n';
     
     int y {6};
     ptr = &y;
+    std::cout<<*ptr<<'\n';
+    
+    *ptr = 6;
     std::cout<<*ptr<<'\n';
     
     return 0;
