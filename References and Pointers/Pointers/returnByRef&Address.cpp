30519074c63345cf160d97f0f6d35b439c1543a5
shade64 <toryokodogbe270@gmail.com>
2024-08-10 16:54:03 +0000

    10 Aug 2024, 17:54

--- References and Pointers/Pointers/returnByRef&Address.cpp
+++ References and Pointers/Pointers/returnByRef&Address.cpp
@@ -4,19 +4,20 @@ std::string returnByValue(std::string text) // expensive
 {
     return text;
 }
-std::string& returnByRef(std::string& text)
+std::string& returnByRef(std::string text)
 {
-    return &text;
+    return text;
 }
 
-std::string* returnByAddress(const std::string* text){
-    return *text
+std::string* returnByAddress(const std::string text){
+    return text
 }
 int main()
 {
-    std::string text{"Hello, World!\n"}
-    //returnByValue(text);
-    //returnByRef(text);
-    //returnByAddress(text);
+    std::string text{"Hello, World!\n"};
+
+    std::cout<<returnByValue(text);
+    std::cout<<returnByRef(text);
+    std::cout<<returnByAddress(text);
     return 0;
 }
