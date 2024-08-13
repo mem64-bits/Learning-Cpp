84882f315120c542c38daee89c369ae724ae66f5
shade64 <toryokodogbe270@gmail.com>
2024-08-13 18:48:51 +0000

    13 Aug 2024, 19:48

--- Challenges/Rock,Paper,Scissors/random.h
+++ Challenges/Rock,Paper,Scissors/random.h
@@ -8,9 +8,9 @@ namespace Random{
 	std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() }; 
 	std::mt19937 mt{ ss }; 
     
-    std::mt19937 randomSelection(int min_range=1 int max_range=6 ){
-    mt = std::uniform_int_distribution{min_range,max_range};
-    return mt;
+    std::mt19937 randomSelection(int min_range int max_range){
+        mt = std::uniform_int_distribution{min_range,max_range};
+        return mt;
     }
 }
 
