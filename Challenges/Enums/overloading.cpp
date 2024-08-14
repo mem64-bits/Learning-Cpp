d1c3913c95528131b93c1238ad368c24452f084c
shade64 <toryokodogbe270@gmail.com>
2024-08-14 22:26:09 +0000

    14 Aug 2024, 23:26

---
+++ Challenges/Enums/overloading.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string_view>
+
+namespace Cars
+{
+    enum CarFuelTypes
+    {
+        petrol,
+        diesel,
+        electric,
+        hydrogen,
+    };
+}
+ 
+constexpr std::string_view getCarFuelType(Cars::CarFuelTypes fueltype){
+
+    switch(fueltype)
+    {
+        case petrol: return "petrol";
+        case diesel: return "diesel";
+        case electric: return "electric";
+        case hydrogen: return "hydrogen";
+        default: return "????";
+    }
+}
+
+std::ostream& operator<<(std::ostream& out, Cars::CarFuelTypes fueltype ){
+    out<<getCarFuelType(out);
+    return out;
+}
+
+
+int main(){
+
+    std::cout<<Cars::CarFuelTypes::diesel;
+
+    return 0;
+}
