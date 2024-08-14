31cb77dbc2ed30bd44aac884c2bcfb0032f09b66
shade64 <toryokodogbe270@gmail.com>
2024-08-14 18:33:11 +0000

    14 Aug 2024, 19:33

---
+++ Enums/enumIntegralConversions.cpp
@@ -0,0 +1,33 @@
+enum Color
+{
+    black,   // 0
+    red,     // 1
+    blue,    // 2
+    green,   // 3
+    white,   // 4
+    cyan,    // 5
+    yellow,  // 6
+    magenta, // 7
+};
+
+
+// You can change values of enums
+// (Not advised)
+
+enum Animal
+{
+    cat = -3,    // values can be negative
+    dog,         // -2
+    pig,         // -1
+    horse = 5,
+    giraffe = 5, // shares same value as horse
+    chicken,     // 6
+};
+
+
+int main()
+{
+    Color shirt{ blue }; // shirt actually stores integral value 2
+
+    return 0;
+}
