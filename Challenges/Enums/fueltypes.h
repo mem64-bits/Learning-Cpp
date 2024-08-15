c6734577db89fa0db8ac9c4553fc3dbe2867d455
shade64 <toryokodogbe270@gmail.com>
2024-08-15 19:44:12 +0000

    15 Aug 2024, 20:44

---
+++ Challenges/Enums/fueltypes.h
@@ -0,0 +1,71 @@
+#ifndef FUEL_TYPES_H
+#define FUEL_TYPES_H
+
+#include <string_view>
+#include <string>
+#include <optional>
+#include <limits>
+
+
+namespace Cars
+{
+    enum CarFuelTypes
+    {
+        petrol,
+        diesel,
+        electric,
+        hydrogen,
+    };
+}
+ 
+inline constexpr std::string_view getCarFuelType(Cars::CarFuelTypes fueltype){
+
+    using namespace Cars;
+    {
+        switch(fueltype)
+        {
+            case petrol : return "petrol";
+            case diesel: return "diesel";
+            case electric: return "electric";
+            case hydrogen: return "hydrogen";
+            default: return "???";
+        }
+    }
+}
+
+inline constexpr std::optional<Cars::CarFuelTypes> getFuelTypeFromString(std::string_view sv)
+{
+    using namespace Cars;
+    {
+        if(sv == "petrol") return petrol;
+        if(sv == "diesel'") return diesel;
+        if(sv == "electric") return electric;
+        if(sv == "hydrogen") return hydrogen;
+    }
+
+    return {};
+}
+
+std::istream& operator>>(std::istream& in, Cars::CarFuelTypes fueltype)
+{
+    std::string s{};
+    in>>s;
+    std::optional<Cars::CarFuelTypes> match{getFuelTypeFromString(s)};
+
+     if (match) // if we found a match
+    {
+        fueltype = *match; // dereference std::optional to get matching enumerator
+        return in;
+    }
+
+    in.setstate(std::ios_base::failbit);
+    return in;
+}
+
+
+std::ostream& operator<<(std::ostream& out, Cars::CarFuelTypes fueltype ){
+    out<<getCarFuelType(fueltype);
+    return out;
+}
+
+#endif
--- Challenges/Enums/overloading.cpp
+++ Challenges/Enums/overloading.cpp
@@ -1,69 +1,6 @@
 #include <iostream>
-#include <string_view>
-#include <string>
-#include <optional>
-#include <limits>
+#include "fueltypes.h"
 
-namespace Cars
-{
-    enum CarFuelTypes
-    {
-        petrol,
-        diesel,
-        electric,
-        hydrogen,
-    };
-}
- 
-constexpr std::string_view getCarFuelType(Cars::CarFuelTypes fueltype){
-
-    using namespace Cars;
-    {
-        switch(fueltype)
-        {
-            case petrol : return "petrol";
-            case diesel: return "diesel";
-            case electric: return "electric";
-            case hydrogen: return "hydrogen";
-            default: return "???";
-        }
-    }
-}
-
-constexpr std::optional<Cars::CarFuelTypes> getFuelTypeFromString(std::string_view sv)
-{
-    using namespace Cars;
-    {
-        if(sv == "petrol") return petrol;
-        if(sv == "diesel'") return diesel;
-        if(sv == "electric") return electric;
-        if(sv == "hydrogen") return hydrogen;
-    }
-
-    return {};
-}
-
-std::istream& operator>>(std::istream& in, Cars::CarFuelTypes fueltype)
-{
-    std::string s{};
-    in>>s;
-    std::optional<Cars::CarFuelTypes> match{getFuelTypeFromString(s)};
-
-     if (match) // if we found a match
-    {
-        fueltype = *match; // dereference std::optional to get matching enumerator
-        return in;
-    }
-
-    in.setstate(std::ios_base::failbit);
-    return in;
-}
-
-
-std::ostream& operator<<(std::ostream& out, Cars::CarFuelTypes fueltype ){
-    out<<getCarFuelType(fueltype);
-    return out;
-}
 
 
 
@@ -77,15 +14,5 @@ int main(){
     Cars::CarFuelTypes car_type{};
     std::cout<<"Enter the car fuel type you have: ";
     std::cin>>car_type; 
-
-    if(car_type  == Cars::electric)
-    {
-        std::cout<<"You must like Teslas, Gay!!!\n";
-    }
-
-    else{
-        std::cout<<"Nice Choice :)\n";
-    }
-
     return 0;
 }
