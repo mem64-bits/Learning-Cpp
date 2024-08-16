fbaf84f796c1582ef6d341ba709f888c841dfd2c
shade64 <toryokodogbe270@gmail.com>
2024-08-16 20:02:27 +0000

    16 Aug 2024, 21:02

---
+++ Challenges/Fractions/Struct Fractions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+struct Fraction
+{
+    int nominator {}
+    int dominnator {};
+};
+
+Fraction getUserFraction(const Fraction& fraction)
+{
+
+}
+
+
+int main(){
+
+    return 0;
+}
