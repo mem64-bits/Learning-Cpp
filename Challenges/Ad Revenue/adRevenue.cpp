32eeb8f86f7e7805cb298bdde47e4edddc2254af
shade64 <toryokodogbe270@gmail.com>
2024-08-16 19:45:08 +0000

    16 Aug 2024, 20:45

--- Challenges/Ad Revenue/adRevenue.cpp
+++ Challenges/Ad Revenue/adRevenue.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 
 
-struct adRevenue
+struct Advertising
 {
     int ads_watched {};
     double ads_clicked_percent {};
@@ -10,16 +10,16 @@ struct adRevenue
 };
 
 
-adRevenue getUserRevenue()
+Advertising getUserRevenue()
 {   
-    adRevenue ad_info{};
-    std::cout<<"Enter in the ads the user has watched: ";
+    Advertising ad_info{};
+    std::cout<<"Number of Ads shown: ";
     std::cin>>ad_info.ads_watched;
 
     std::cout<<"\nEnter in the pecentage of ads the user has clicked: ";
     std::cin>>ad_info.ads_clicked_percent;
 
-    std::cout<<"\nEnter in the average earnings of the ads: ";
+    std::cout<<"\nEnter in the average earnings of the ads per click: ";
     std::cin>>ad_info.ads_av_earnings;
     std::cout<<'\n';
 
@@ -27,20 +27,20 @@ adRevenue getUserRevenue()
 
 }
 
-adRevenue printAdRevenue(const adRevenue& ad){
+Advertising printAdvertising(const Advertising& ad){
     std::cout<<ad.ads_watched<<'\n';
     std::cout<<ad.ads_clicked_percent<<'\n';
     std::cout<<ad.ads_av_earnings<<'\n';
 
     double total_daily_earnings{ad.ads_watched*(ad.ads_clicked_percent/100)*ad.ads_av_earnings};
     std::setprecision(2);
-    std::cout<<"The total daily ad revenue is: "<<total_daily_earnings;
+    std::cout<<"Total Revenue: "<<"€"<<total_daily_earnings;
 
     return {};
 }
 
 
 int main(){
-    printAdRevenue(getUserRevenue());
+    printAdvertising(getUserRevenue());
     return 0;
 }
