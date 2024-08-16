418c03a773fc3fcebfd2b0f9317ea1b6303c5979
shade64 <toryokodogbe270@gmail.com>
2024-08-16 20:32:11 +0000

    16 Aug 2024, 21:32

---
+++ Challenges/Fractions/structFractions.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+struct Fraction
+{
+    int numerator {};
+    char seperator{'/'};
+    int denominator {};
+};
+
+Fraction getUserFraction()
+{   
+    Fraction fraction{};
+    std::cout<<"Enter a value for the numerator: ";
+    std::cin>>fraction.numerator;
+
+    std::cout<<"\nEnter a value for the denominator: ";
+    std::cin>>fraction.denominator;
+    std::cout<<'\n'<<'\n';
+    return fraction;
+}
+
+Fraction fractMultiply(const Fraction& fraction_1, const Fraction& fraction_2)
+{
+    Fraction result{(fraction_1.numerator*fraction_2.numerator),(fraction_1.denominator*fraction_2.denominator)};
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fraction& fraction)
+{
+    out<<fraction.numerator<<fraction.seperator<<fraction.denominator;
+    return out;
+}
+
+int main(){
+    Fraction result {fractMultiply(getUserFraction(),getUserFraction())};
+    std::cout<<"Your fractions multiplied together: "<<result;
+    return 0;
+}
