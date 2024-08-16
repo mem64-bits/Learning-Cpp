ce813a33c300dc1de84791ff22c77acd269c4801
shade64 <toryokodogbe270@gmail.com>
2024-08-16 22:08:28 +0000

    16 Aug 2024, 23:08

---
+++ Structs/structSize.cpp
@@ -0,0 +1,24 @@
+/*Most of the time the sixe of a struct is the sum 
+of all the members in it, but this isn't always the case 
+due to the compiler adding gaps called padding*/
+
+
+#include <iostream>
+
+struct Foo
+{
+    short a {};
+    int b {};
+    double c {};
+};
+
+int main()
+{
+    std::cout << "The size of short is " << sizeof(short) << " bytes\n";
+    std::cout << "The size of int is " << sizeof(int) << " bytes\n";
+    std::cout << "The size of double is " << sizeof(double) << " bytes\n";
+
+    std::cout << "The size of Foo is " << sizeof(Foo) << " bytes\n";
+
+    return 0;
+}
