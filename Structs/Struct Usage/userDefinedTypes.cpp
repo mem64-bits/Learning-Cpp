28ca58006e33f52891de340c881aac6526db1eab
shade64 <toryokodogbe270@gmail.com>
2024-08-18 19:40:16 +0000

    18 Aug 2024, 20:40

--- Structs/Struct Usage/userDefinedTypes.cpp
+++ Structs/Struct Usage/userDefinedTypes.cpp
@@ -4,15 +4,23 @@
 
 // you can use structs in header files 
 struct Fraction
-{   int nominator{};
+{   int numerator{};
     int denominator{};  
 };
 
+
+// Overloads << to allow printing of type Fraction
+std::ostream& operator<<(std::ostream& out, const Fraction& f){
+    out<<f.numerator<<"/"<<f.denominator;
+    return out;
+}
 
     
 int main(){
 Fraction f{3,4};
 // makes an object of the type Fraction
+
+
 
 std::cout<<f;
     return 0;
