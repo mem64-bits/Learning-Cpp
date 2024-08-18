c5a826169ff027cf1a23fe9aedb3f65bcb4ddea8
shade64 <toryokodogbe270@gmail.com>
2024-08-18 18:28:24 +0000

    18 Aug 2024, 19:28

--- Structs/avoidPadding.cpp
+++ Structs/avoidPadding.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+
+/*compiler which add padding, making 
+the struct's byte size bigger*/
+
 struct PaddedStruct
 {
     short a{};
     int b{};
     short c{};
 };
+
+/*Order Structs form biggest 
+to smallest to minimize padding*/
 
 struct UnpaddedStruct
 {
@@ -17,7 +24,9 @@ struct UnpaddedStruct
 
 int main()
 {   
+//  prints 12
     std::cout<<sizeof(PaddedStruct)<<'\n';
+// prints 8
     std::cout<<sizeof(UnpaddedStruct)<<'\n';
     return 0;
 }
