e5ad77155a07424b4613f30ff59adc1fc4b31eca
shade64 <toryokodogbe270@gmail.com>
2024-08-18 22:36:04 +0000

    18 Aug 2024, 23:36

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -12,8 +12,17 @@ void rotate(Vec3& point, float x=1, float y=1, float z=1)
     float rad{0};
 
     rad = x;
-    point.y {std::cos(rad) * point.y - std::sin(rad) * point.z;
+    point.y = std::cos(rad) * point.y - std::sin(rad) * point.z;
     point.z = std::sin(rad) * point.y + std::cos(rad) * point.z;
+
+    rad = y;
+    point.x = std::cos(rad) * point.x + std::sin(rad) * point.z;
+    point.y = -std::sin(rad) * point.x + std::cos(rad) * point.z;
+
+    rad = z;
+    point.x = std::cos(rad) * point.x + std::sin(rad) * point.z;
+
+
 
 }
 
