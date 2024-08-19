1ed5249dcba8b5aae6c38a404beca0a4e132c7c1
shade64 <toryokodogbe270@gmail.com>
2024-08-19 10:38:20 +0000

    19 Aug 2024, 11:38

--- Graphics/rotatingCubeFixed.cpp
+++ Graphics/rotatingCubeFixed.cpp
@@ -42,4 +42,108 @@ struct Vec3
     }
 };
 
+struct Connection
+{
+    int a,b;
+};
 
+void line(Screen& screen, float x1, float y1, float x2, float y2)
+{
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float length = std::sqrt(dx * dx + dy * dy);
+    float angle = std::atan2(dy,dx);
+
+    for(float i = 0; i < length; i++ )
+    {
+        screen.pixel(
+                        x1 + std::cos(angle) * i,
+                        y1 + std::sin(angle) * i
+        );
+    }
+}
+
+int main()
+{
+    Screen screen;
+
+    std::vector<Vec3> points {
+        {100,100,100},
+        {200,100,100},
+        {200,200,100},
+        {100,200,100},
+        
+        {100,100,200},
+        {200,100,200},
+        {200,200,200},
+        {100,200,200}
+    };
+
+    std::vector<Connection> connections
+    {
+        {0,4},
+        {1,5},
+        {2,6},
+        {3,7},
+
+        {0,1},
+        {1,2},
+        {2,3},
+        {3,0},
+
+        {4,5},
+        {5,6},
+        {6,7},
+        {7,4}
+    };
+
+    Vec3 c{0,0,0};
+    
+    for(auto& p : points)
+    {
+        c.x += p.x;
+        c.y += p.y;
+        c.z += p.z;
+    }
+
+    c.x /= points.size();  
+    c.y /= points.size();
+    c.z /= points.size();
+
+    while(true)
+    {   
+        for(auto& p : points)
+        {
+            // Translate points to origin
+            p.x -= c.x;
+            p.y -= c.y;
+            p.z -= c.z;
+
+            // Rotate the points
+            p.rotate(0.02, 0.01, 0.04);
+
+            // Translate points back to original position
+            p.x += c.x;
+            p.y += c.y;
+            p.z += c.z;
+
+            // Draw the point
+            screen.pixel(p.x, p.y);
+        }
+
+        for(auto& conn : connections)
+        {
+            line(screen, points[conn.a].x,
+                         points[conn.a].y,
+                         points[conn.b].x,
+                         points[conn.b].y);
+        }
+
+        screen.show();
+        screen.clear();
+        screen.input();
+        SDL_Delay(30);
+    }
+
+    return 0;
+}
