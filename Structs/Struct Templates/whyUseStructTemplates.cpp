264e7df5dc6ac2788e8d0e21f7efc0b2ca23a15b
shade64 <toryokodogbe270@gmail.com>
2024-08-20 19:27:21 +0000

    20 Aug 2024, 20:27

---
+++ Structs/Struct Templates/whyUseStructTemplates.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+// defines a struct
+struct Pair
+{
+    int first {};
+    int second {};
+};
+
+// We might want to change the type of the members in the structs later in the program
+
+struct Pair // compile error: erroneous redefinition of Pair
+{
+    double first{};
+    double second{};
+};
+
+// function to compare struct members of an int type
+
+constexpr int max(Pair p)
+{
+    return (p.first < p.second ? p.second : p.first);
+}
+
+
+// The  overload doesnt work because the compiler can't tell which struct 
+// to use based on the return type alone
+
+#if 0
+constexpr double max(Pair p)
+{
+    return (p.first < p.second ? p.second : p.first);
+}
+#endif
+
+
+
+int main()
+{
+    Pair p1{ 5, 6 };
+    Pair p2 {5.0f,6.0f};
+
+    std::cout << max(p1) << " is larger\n";
+    std::cout<<max(p2) << " is larger\n";
+    return 0;
+}
