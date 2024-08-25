6a99834b624d056ef542de5fe0335cab52f16a72
shade64 <toryokodogbe270@gmail.com>
2024-08-25 22:13:55 +0000

    25 Aug 2024, 23:13

---
+++ Structs/Class Templates/Class Template Headers/classTemplateDeduction.cpp
@@ -0,0 +1,22 @@
+#include <utility> // contains std::pair
+#include <typeinfo>
+#include <iostream>
+
+// Class Arguement Deduction (CTAD)
+
+/* From C++ 17 onwards the compiler can automatically
+deduct the type of class template objects from the
+ initializers*/
+
+int main()
+{
+// explictily specify class template C++11 to before C++17)
+    std::pair<int,int> p1{1,2};
+
+// CTAD automatically gets the type of class template 
+    std::pair p2{1,2};
+    
+    std::cout<<"Type of p1: " << typeid(p1).name()<<'\n';
+    std::cout<<"Type of p2: "<< typeid(p2).name()<<'\n';
+    return 0;
+}
--- Structs/Class Templates/Class Template Headers/main.cpp
+++ Structs/Class Templates/Class Template Headers/main.cpp
@@ -8,7 +8,7 @@ void foo();
 int main()
 {
     Pair<double> p2 {3.4, 5.6};
-    std::cout<<max(p2)<<" is larger than\n";
+    std::cout<<max(p2)<<" is larger\n";
     foo();
     return 0;
 }
