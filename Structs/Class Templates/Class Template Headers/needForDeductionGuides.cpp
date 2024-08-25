298810f26ef9babc9580e026003cd7934d1f3204
shade64 <toryokodogbe270@gmail.com>
2024-08-25 22:38:18 +0000

    25 Aug 2024, 23:38

---
+++ Structs/Class Templates/Class Template Headers/needForDeductionGuides.cpp
@@ -0,0 +1,20 @@
+// define our own Pair Type
+template <typename T, typename U>
+struct Pair
+{
+    T first{};
+    U second{};
+};
+
+int main()
+{
+/* compiles okay both types are specifed 
+    in the template arguements*/
+    Pair<int,int> p1{1,2};
+
+    // does not compile in C++17 (compiles in C++20)
+    Pair p2{1,2};
+    return 0;
+
+}
+
