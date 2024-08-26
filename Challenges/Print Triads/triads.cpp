842b705e82c844c4eaeaf80f7027aacb0f31d09e
shade64 <toryokodogbe270@gmail.com>
2024-08-26 21:52:15 +0000

    26 Aug 2024, 22:52

--- Challenges/Print Triads/triads.cpp
+++ Challenges/Print Triads/triads.cpp
@@ -9,17 +9,19 @@ struct Triad
 };
 
 template <typename T>
-void print(Triad<T> t)
+void print(Triad<T>& t)
 {
     std::cout<<"["<<t.first<<", " <<t.second<<", "<<t.third<<"]";
 }
 
 template <typename T>
-Triad(T)->Triad<T>;
+Triad(T,T,T)->Triad<T>;
 
 int main()
 {
     Triad<int> t1{ 1, 2, 3 };
     Triad<double> t2{ 1.2, 3.4, 5.6 };
+    print(t1);
+    print(t2);
     return 0;
 }
