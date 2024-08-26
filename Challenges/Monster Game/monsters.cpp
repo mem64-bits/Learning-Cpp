688318bd6aa6dfebd139cb9289c2afe20e6c45b8
shade64 <toryokodogbe270@gmail.com>
2024-08-26 21:08:24 +0000

    26 Aug 2024, 22:08

--- Challenges/Monster Game/monsters.cpp
+++ Challenges/Monster Game/monsters.cpp
@@ -1,60 +1,52 @@
 #include <string_view>
 #include<iostream>
 
-enum MonsterType
-{
-    Orge,
-    Dragon,
-    Orc,
-    Giant_Spider,
-    Slime,
-};
-
 
 struct Monster
 {
-    MonsterType monster_type {};
+
+    enum Type
+    {
+    ogre,
+    dragon,
+    orc,
+    giant_spider,
+    slime,
+    };
+
+    Type type {};
     std::string_view monster_name {};
     int monster_health {};
 };
 
-void printMonster(Monster m)
+
+constexpr std::string_view monsterTypeToString(Monster::Type type)
+{
+    switch (type)
+	{
+	case Monster::ogre:          return "Ogre";
+	case Monster::dragon:        return "Dragon";
+	case Monster::orc:           return "Orc";
+	case Monster::giant_spider:  return "Giant Spider";
+	case Monster::slime:         return "Slime";
+	}
+
+	return "Unknown";
+}
+
+void printMonster(Monster& m)
 {   
-    std::string_view monster_type {};
     
-    using enum MonsterType;
-    {
-        switch(m.monster_type)
-        {
-            case Orge: monster_type = "Orge"; 
-                break;
-
-            case Dragon: monster_type = "Dragon"; 
-                break;
-
-            case Orc: monster_type = "Orc";
-                break;
-
-            case Giant_Spider: monster_type = "Giant Spider";
-                break;
-
-            case Slime : monster_type = "Slime";
-                break;
-
-            default : monster_type = "???";
-                break;
-
-        } 
-    }
     
-    std::cout<<"This "<<monster_type<<" is named "<<m.monster_name<<
+    
+    std::cout<<"This "<<monsterTypeToString(m.type)<<" is named "<<m.monster_name<<
     " and has "<<m.monster_health<< " health.\n";
 }
 
 int main()
 {
-    Monster mon_1 {MonsterType::Orge,"Torg",145};
-    Monster mon_2 {MonsterType::Slime,"Blurp",23};
+    Monster mon_1 {Monster::ogre,"Torg",145};
+    Monster mon_2 {Monster::slime,"Blurp",23};
     printMonster(mon_1);
     printMonster(mon_2);
     return 0;
