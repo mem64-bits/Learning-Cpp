4086f96e5c33b0789545e9bfc854018e8b3128cc
shade64 <toryokodogbe270@gmail.com>
2024-08-27 00:11:23 +0000

    27 Aug 2024, 01:11

--- Object Oriented Programming/Classes/memberFunctions.cpp
+++ Object Oriented Programming/Classes/memberFunctions.cpp
@@ -10,14 +10,24 @@ public:
 
     void print()
     {
-        std::cout<<year<<'/'<<month<<'/'<<day;
+        std::cout<<year<<'/'<<month<<'/'<<day<<'\n';
     }
 };
+
+
+// non-member version of print
+void non_member_print(Date& date)
+{
+     // member variables accessed using member selection operator (.)
+    std::cout<<date.year<<'/'<<date.month<<'/'<<date.day<<'\n';
+}
 
 int main()
 {
     Date today{24,8,27};
     today.day = 16;
     today.print();
+    non_member_print(today);
+
     return 0;
 }
