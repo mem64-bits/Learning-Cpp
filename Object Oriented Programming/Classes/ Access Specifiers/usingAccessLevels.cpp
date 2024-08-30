cbe3f033462f3112cbde1ac7db56162249464b06
shade64 <toryokodogbe270@gmail.com>
2024-08-30 23:16:57 +0000

    31 Aug 2024, 00:16

--- Object Oriented Programming/Classes/ Access Specifiers/usingAccessLevels.cpp
+++ Object Oriented Programming/Classes/ Access Specifiers/usingAccessLevels.cpp
@@ -6,16 +6,23 @@ class Person
 {
 // things under private access specifer can't be accessed directly outside of the class 
 private:
+    // m_name is private
     std::string m_name {};
 
 public:
+// kisses is a public function
     void kisses(Person& p)
-    {
+    {   
+
+ /*objects under public access specifer 
+can still access private objects*/
+
         std::cout<<m_name<<" kisses "<<p.m_name<<'\n';
     }
 
     void setName(std::string_view name)
-    {
+    {   
+        // public function can modify value of private function
         m_name = name;
     }
 };
---
+++ Object Oriented Programming/Classes/Questions/point3D.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Point3D
+{
+private:
+    int m_x {};
+    int m_y {};
+    int m_z {};
+
+public:
+    void print()
+    {
+        std::cout<<'<'<<m_x<<','<<m_y<<','<<m_z<<'>';
+    }
+
+    void setValues(int x,int y,int z)
+    {
+        m_x = x;
+        m_y  = y;
+        m_z = z;
+    }
+};
+
+int main()
+{
+    Point3D point;
+    point.setValues(1,2,3);
+    point.print();
+    std::cout<<'\n';
+
+    return 0;
+}
