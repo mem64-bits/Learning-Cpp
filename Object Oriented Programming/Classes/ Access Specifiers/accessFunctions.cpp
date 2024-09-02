92e6dd01c94bbbf4fc60561a23c8eafc8cf24cc1
shade64 <toryokodogbe270@gmail.com>
2024-09-02 14:42:11 +0000

    2 Sept 2024, 15:42

---
+++ Object Oriented Programming/Classes/ Access Specifiers/accessFunctions.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+/* An access function is a trivial public
+ member function whose job is to retrieve 
+ or change the value of a private member variable.
+ 
+ 
+ They can be split into to groups called getters and setters*/
+
+
+
+class Date
+{
+private:
+
+   int m_year {};
+   int m_month {};
+   int m_day {};
+
+public: 
+
+// getter for year
+    int getYear() const
+    {
+        return m_year;
+    } 
+
+//  setter for year
+    void setYear(int year)
+    {
+        m_year = year;
+    }
+
+// getter for month
+    int getMonth()
+    {
+        return m_month;
+    }
+
+//   setter for month
+    void setMonth(int month)
+    {
+        m_month = month;
+    }
+
+//  getter for day
+    int getDay()
+    {
+        return m_day;
+    }
+
+// setter for day 
+    void setDay(int day)
+    {    
+        m_day = day;
+    }
+};
+
+int main()
+{
+    Date d{};
+    d.setYear(2021);
+    std::cout<<"The year is "<<d.getYear();
+    return 0;
+}
