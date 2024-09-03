c2861f912fac77928463356065c52721f3db0969
shade64 <toryokodogbe270@gmail.com>
2024-09-03 23:57:51 +0000

    4 Sept 2024, 00:57

--- Object Oriented Programming/Classes/Why Data Hiding Is Good/changingImplementation.cpp
+++ Object Oriented Programming/Classes/Why Data Hiding Is Good/changingImplementation.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
-struct Something
-
+class Something
 {
-    int value1 {};
-    int value2 {};
-    int value3 {};
-};
+private:
+    int m_value[3]; // note: we changed the implementation of this class!
 
+public:
+    // We have to update any member functions to reflect the new implementation
+    void setValue1(int value) { m_value[0] = value; }
+    int getValue1() const { return m_value[0]; }
+};
 
 int main()
 {
+    // But our programs that use the class do not need to be updated!
     Something something;
-    something.value1 = 5;
-    std::cout<<something.value1 << '\n';
-    return 0;
+    something.setValue1(5);
+    std::cout << something.getValue1() << '\n';
 }
