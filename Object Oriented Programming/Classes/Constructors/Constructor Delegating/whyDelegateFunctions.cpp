d06730004f455c1f47d7c9b23c8739379faa1cee
shade64 <toryokodogbe270@gmail.com>
2024-09-04 18:52:05 +0000

    4 Sept 2024, 19:52

---
+++ Object Oriented Programming/Classes/Constructors/Constructor Delegating/whyDelegateFunctions.cpp
@@ -0,0 +1,24 @@
+/*Delegating functions helps 
+reduce redundant Code*/
+
+void A()
+{
+    // statements that do task A
+}
+
+void B()
+{
+    // statements that do task A
+    // statements that do task B
+}
+
+void A()
+{
+    // statements that do task A
+}
+
+void B()
+{
+    A();
+    // statements that do task B
+}
