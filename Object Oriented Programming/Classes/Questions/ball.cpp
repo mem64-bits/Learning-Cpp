3c33fb39766eda4faf5f521c76c3b660360b1771
shade64 <toryokodogbe270@gmail.com>
2024-09-04 12:11:05 +0000

    4 Sept 2024, 13:11

--- Object Oriented Programming/Classes/Questions/ball.cpp
+++ Object Oriented Programming/Classes/Questions/ball.cpp
@@ -7,19 +7,40 @@ public:
 
     // Constructor for ball class
     Ball(std::string_view colour, double radius)
-    :m_colour{colour}, m_radius{radius}
+    : m_colour{colour}, 
+      m_radius{radius}
     { 
         std::cout<<"Ball("<<colour<<", "<<radius<<")\n";
     }
 
+    const std::string& getColour() const
+    {
+        return m_colour;
+    }
+
+    double getRadius() const
+    {
+        return m_radius;
+    }
+
+ 
 private:
     std::string m_colour  {};
     double m_radius {};   
 };
 
+
+void print(const Ball& ball )
+{
+    std::cout<<"Ball("<<ball.getColour()<<", "<<ball.getRadius()<<")\n";
+}
+
 int main()
 {
-    Ball{"blue", 10.0};
-    Ball{"red", 12.0};
+    Ball blue{"blue", 10.0};
+    print(blue);
+    
+    Ball red {"red", 12.0};
+    print(red);
     return 0;
 }
