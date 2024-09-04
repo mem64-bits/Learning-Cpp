2ecde9381852b486b12ff1e9fdcf48fcd18cce5b
shade64 <toryokodogbe270@gmail.com>
2024-09-04 13:20:33 +0000

    4 Sept 2024, 14:20

---
+++ Object Oriented Programming/Classes/Constructors/Overloading Constructors/overloadedConstructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+/*since Constructors are functions 
+they can also be overloaded*/
+class Foo
+{
+private:
+    int m_x {};
+    int m_y {};
+
+public:
+    Foo() // default constructor
+    {
+        std::cout<<"Foo constructed"; 
+    }
+
+    Foo(int x, int y)
+    :m_x{x}, m_y{y}
+    {
+        std::cout<<"Foo("<<m_x<<", "<<m_y<<") constructed\n";
+    }
+};
+
+int main()
+{
+    Foo foo1{}; // Calls Foo() constructor
+    Foo foo2{6,7}; // Calls overloaded Foo(int,int)
+    return 0;
+}
