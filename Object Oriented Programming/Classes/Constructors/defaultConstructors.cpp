ac488162b76ed1a927c085e80c3399abeec359e2
shade64 <toryokodogbe270@gmail.com>
2024-09-04 12:32:11 +0000

    4 Sept 2024, 13:32

--- Object Oriented Programming/Classes/Constructors/defaultConstructors.cpp
+++ Object Oriented Programming/Classes/Constructors/defaultConstructors.cpp
@@ -5,7 +5,11 @@ class Foo
 /*A default constructor is a constructor that accepts no arguments. Typically,
  this is a constructor that has been defined with no parameters.*/
 {
+
 public:
+/*Prefer value initialization over default 
+initialization for all class types.*/
+
     Foo() // default constructor
     {
         std::cout<<"Foo default Constructor";
