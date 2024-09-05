8a018ff078d73d5c69e76cd661b5e6b0f7017e14
shade64 <toryokodogbe270@gmail.com>
2024-09-05 20:29:23 +0000

    5 Sept 2024, 21:29

--- Object Oriented Programming/Classes/Constructors/Constructor Delegating/delegatingConstructors.cpp
+++ Object Oriented Programming/Classes/Constructors/Constructor Delegating/delegatingConstructors.cpp
@@ -10,6 +10,8 @@ private:
 
 public:
 // Constructor for employee
+//--------------------------
+// Delegating constructor
     Employee(std::string_view name)
     /*delegates initialization to 
     Employee(std::string_view, int) constructor*/
@@ -17,18 +19,27 @@ public:
     {
     }
 
+    // We need seperate functions for delegating and initilization
     Employee(std::string_view name, int id)
         :m_name{name}, m_id{id}
     {
         std::cout<<"Employee "<<m_name<<" Created\n";
     }
 
-
 };
 
 
 int main()
 {
-    Employee e1{"James"};
+    Employee e1{"James"}; // name = "James" and id = 0
     Employee e2{"Dave", 42};
 }
+
+/*
+Downsides to delegating
+    Can sometimes make copies of initilizers
+
+    If a delegated function delegates to a another delegated function 
+    it will cause an infinite loop filling memory and causing 
+    the program to crash.
+*/
---
+++ Object Oriented Programming/Classes/Constructors/Constructor Delegating/usingDefaultArgsToReduceConstructors.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+class Employee
+{
+private:
+    std::string m_name {};
+    int m_id {};
+
+public:
+    Employee(std::string_view name, int id = 0)
+    :m_name{name},m_id{id}
+    {
+        std::cout<<"Employee "<<m_name<< "Created"<<"\n";
+    }
+};
+
+int main()
+{
+    Employee e1{"John"};
+    Employee e2{"Joe"};
+    return 0;
+}
