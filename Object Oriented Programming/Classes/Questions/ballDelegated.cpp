a04d326f6612b96ebf661cdae13562c705361187
shade64 <toryokodogbe270@gmail.com>
2024-09-06 18:49:39 +0000

    6 Sept 2024, 19:49

--- Object Oriented Programming/Classes/Questions/ballDelegated.cpp
+++ Object Oriented Programming/Classes/Questions/ballDelegated.cpp
@@ -4,7 +4,7 @@
 class Ball
 {
 public:
-    Ball(std::string_view colour="Black", double radius=10)
+    Ball(std::string_view colour, double radius)
     :   m_colour{colour},
         m_radius{radius}
 
@@ -12,21 +12,23 @@ public:
         std::cout<<"Ball("<<m_colour<<", "<<m_radius<<")\n";
     }
 
+    Ball()
+    {
+    }
+
 
 
 private:
-    std::string m_colour {};
-    double m_radius {};
-    
-
+    std::string m_colour {"Black"};
+    double m_radius {"10"};
 };
 
 int main()
 {
     Ball def{};
+    //Ball blue{"blue"};
+    //Ball twenty{ 20.0 };
     Ball blueTwenty{"blue",20.0};
-
-    return 0;
 
     return 0;
 }
