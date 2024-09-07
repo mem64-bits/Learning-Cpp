8fede534aa54768c01090d79f8c9b048f37a5fd5
shade64 <toryokodogbe270@gmail.com>
2024-09-07 19:31:55 +0100

    Classes last Questions

--- Object Oriented Programming/Classes/Questions/fractionMultiply.cpp
+++ Object Oriented Programming/Classes/Questions/fractionMultiply.cpp
@@ -2,43 +2,67 @@
 
 class Fraction
 {
+private: 
+    int m_numerator{0};
+    int m_denominator{1};
+
 public:
-    Fraction multiply( const Fraction& f)
-    {
-    return {numerator * f.numerator, denominator * f.denominator };
-    }
-private:
-    int numerator{ 0 };
-    int denominator{ 1 };
-};
+
+int getNumerator() const {return m_numerator;}
+int getDenominator() const {return m_denominator;}
+
+void setNumerator(const int& numerator)
+{
+    m_numerator = numerator;
+}
+
+void setDenominator(const int& denominator) 
+{
+    m_denominator = denominator;
+}
+
+
+
+Fraction(int numerator=0, int denominator=1)
+: m_numerator{numerator}, m_denominator{denominator}
+{
+}
+
+void printFraction() const
+{
+    std::cout<<getNumerator()<<'/'<<getDenominator()<<'\n';
+}
+
+ Fraction multiply (const Fraction& f )
+{
+    return Fraction{ m_numerator * f.m_numerator, m_denominator * f.m_denominator };
+}
 
 Fraction getFraction()
 {
     Fraction temp{};
     std::cout << "Enter a value for numerator: ";
-    std::cin >> temp.numerator;
+    std::cin >> temp.m_numerator;
     std::cout << "Enter a value for denominator: ";
-    std::cin >> temp.denominator;
+    std::cin >> temp.m_denominator;
     std::cout << '\n';
 
     return temp;
 }
 
+};
 
 
-void printFraction(const Fraction& f)
-{
-    std::cout << f.numerator << '/' << f.denominator << '\n';
-}
 
 int main()
 {
-    Fraction f1{ getFraction() };
-    Fraction f2{ getFraction() };
+    Fraction f1 {};
+    Fraction f2 {};
+
+    f1.getFraction();
+    f2.getFraction();
 
     std::cout << "Your fractions multiplied together: ";
-
-    printFraction(multiply(f1, f2));
-
+    f1.multiply(f2).printFraction();
     return 0;
 }
