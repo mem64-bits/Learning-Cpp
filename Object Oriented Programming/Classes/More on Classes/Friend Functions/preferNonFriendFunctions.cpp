7c63dae5b4ca910456eec13166537f5898273ad2
shade64 <toryokodogbe270@gmail.com>
2024-09-09 21:59:37 +0000

    9 Sept 2024, 22:59

---
+++ Object Oriented Programming/Classes/More on Classes/Friend Functions/preferNonFriendFunctions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Accumulator
+{
+private:
+    int m_value { 0 }; // if we rename this
+
+public:
+    void add(int value) { m_value += value; } // we need to modify this
+
+    friend void print(const Accumulator& accumulator);
+};
+
+void print(const Accumulator& accumulator)
+{
+    std::cout << accumulator.m_value; // and we need to modify this
+}
+
+int main()
+{
+    Accumulator acc{};
+    acc.add(5); // add 5 to the accumulator
+
+    print(acc); // call the print() non-member function
+
+    return 0;
+}
