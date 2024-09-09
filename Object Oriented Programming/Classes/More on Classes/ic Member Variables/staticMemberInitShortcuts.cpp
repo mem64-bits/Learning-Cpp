1acc90304dad5dc1c7347ec47168e9972096d545
shade64 <toryokodogbe270@gmail.com>
2024-09-09 13:54:44 +0000

    9 Sept 2024, 14:54

--- Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticMemberInitShortcuts.cpp
+++ Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticMemberInitShortcuts.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
+
 /*These are common ways to initilize static members
 inside the class inline or constexpr is prefered*/
-
 
 class Whatever
 {
@@ -9,9 +10,17 @@ public:
     static const int s_value { 4 }; 
 
 // a static inline variable can be defined and initialized directly
-    static inline int s_ {5};
+    static inline int s_counter {1};
 
 /*since constexpr variables are implicitly inline they don't 
 need the inline keyword to be defined and initilized directly*/
     static constexpr double s_height {1.95};
 };
+
+int main()
+{
+    std::cout<<Whatever::s_value<<'\n';
+    std::cout<<Whatever::s_value<<'\n';
+    std::cout<<Whatever::s_value<<'\n';
+    return 0;
+}
