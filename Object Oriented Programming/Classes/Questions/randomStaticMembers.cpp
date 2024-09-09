73e3a848f0c00e3feb36e2b8bff5f6b6b216c979
shade64 <toryokodogbe270@gmail.com>
2024-09-09 17:38:56 +0000

    9 Sept 2024, 18:38

--- Object Oriented Programming/Classes/Questions/randomStaticMembers.cpp
+++ Object Oriented Programming/Classes/Questions/randomStaticMembers.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include <iostream>
+
+class Random
+{
+public:
+    std::mt19937 generate()
+    {
+        std::random_device rd{};
+        // Create seed_seq with high-res clock and 7 random numbers from std::random_device
+		std::seed_seq ss{
+			static_cast<std::seed_seq::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()),
+				rd(), rd(), rd(), rd(), rd(), rd(), rd() };
+
+		return std::mt19937{ ss };
+    }
+};
