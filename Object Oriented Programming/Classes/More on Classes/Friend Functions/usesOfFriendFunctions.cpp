c9e3f3363223d1c988949dddca65f724304590fd
shade64 <toryokodogbe270@gmail.com>
2024-09-09 21:34:01 +0000

    9 Sept 2024, 22:34

--- Object Oriented Programming/Classes/More on Classes/Friend Functions/usesOfFriendFunctions.cpp
+++ Object Oriented Programming/Classes/More on Classes/Friend Functions/usesOfFriendFunctions.cpp
@@ -3,7 +3,7 @@
 /* The friend keyword is used to tell the compiler
 that other class or function is now a friend.
 
-This means the function has full access to public 
+This means the friend function has full access to public 
 and private and protected members of the class */
 int main()
 {
