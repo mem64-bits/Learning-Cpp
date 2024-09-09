7b4d079d9d77b374d528fe1f63786e5a0adc72c0
shade64 <toryokodogbe270@gmail.com>
2024-09-09 10:56:02 +0000

    9 Sept 2024, 11:56

--- Object Oriented Programming/Classes/Questions/triadClassTemplate.cpp
+++ Object Oriented Programming/Classes/Questions/triadClassTemplate.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 #include <string>
 
-template <typename T,T,T>
+template <typename T, typename U, typename V>
 class Triad
 {
 private:
     T m_first{};
-    T m_second{};
-    T m_third{};
+    U m_second{};
+    V m_third{};
 
 public:
-    Triad(T first, T second, T third)
+    Triad(T first, U second, V third)
     : m_first{first}
     , m_second{second}
     , m_third{third}
     {
     }
+    
+    T first() const{return m_first;}
+    T second() const{return m_second;}
+    T third() const{return m_third;}
 
-    T getFirstTraid() const{return m_first;}
-    T getSecondTraid() const{return m_second;}
-    T getThirdTraid() const{return m_third;}
-
-    void print()
+    void print() const
     {
-        std::cout<<'['<<m_first<<", "<<m_second<<", "<<m_third<<"]"
+        std::cout<<'['<<m_first<<", "<<m_second<<", "<<m_third<<"]\n";
     }
 
 };
+
+int main()
+{
+	Triad<int, int, int> t1{ 1, 2, 3 };
+	t1.print();
+	std::cout << '\n';
+	std::cout << t1.first() << '\n';
+
+	using namespace std::literals::string_literals;
+	const Triad t2{ 1, 2.3, "Hello"s };
+	t2.print();
+	std::cout << '\n';
+
+	return 0;
+}
