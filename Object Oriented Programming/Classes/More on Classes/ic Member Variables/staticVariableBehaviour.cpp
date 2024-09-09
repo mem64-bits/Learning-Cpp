7b29c60951182d4035f9a8661c8b7d7029ca21d9
shade64 <toryokodogbe270@gmail.com>
2024-09-09 13:34:50 +0000

    9 Sept 2024, 14:34

--- Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticVariableBehaviour.cpp
+++ Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticVariableBehaviour.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+
 /*Static variables exist independently of the class
-objects*/
+objects and can be accessed directly without the use of an
+object*/
+
+
+class Something
+{
+public:
+    static int s_value; // declares static variable
+};
+
+int Something::s_value{1}; //defines the static member variable
+
+
+int main()
+{
+    // no object of type Something made
+    // uses :: operator to access static member object
+    Something::s_value = 2;
+    std::cout<<Something::s_value<<'\n';
+    return 0;
+}
