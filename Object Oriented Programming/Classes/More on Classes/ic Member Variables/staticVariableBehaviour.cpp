b8b4529fd61b91b212e9fbc496520fb8f213dd25
shade64 <toryokodogbe270@gmail.com>
2024-09-09 13:36:48 +0000

    9 Sept 2024, 14:36

--- Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticVariableBehaviour.cpp
+++ Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticVariableBehaviour.cpp
@@ -11,6 +11,11 @@ public:
     static int s_value; // declares static variable
 };
 
+/*This line serves two purposes: it instantiates the static member 
+variable (just like a global variable), and initializes it. 
+If no initializer is provided, static member variables 
+are zero-initialized by default.*/
+
 int Something::s_value{1}; //defines the static member variable
 
 
