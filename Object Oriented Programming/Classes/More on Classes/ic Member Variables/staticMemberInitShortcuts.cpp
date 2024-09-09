f8f69497b7c586b1ddc79afd6154f1011c21d6c7
shade64 <toryokodogbe270@gmail.com>
2024-09-09 13:51:52 +0000

    9 Sept 2024, 14:51

--- Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticMemberInitShortcuts.cpp
+++ Object Oriented Programming/Classes/More on Classes/ic Member Variables/staticMemberInitShortcuts.cpp
@@ -5,5 +5,13 @@ inside the class inline or constexpr is prefered*/
 class Whatever
 {
 public:
-    static const int s_value{ 4 }; // a static const int can be defined and initialized directly
+// a static const int can be defined and initialized directly
+    static const int s_value { 4 }; 
+
+// a static inline variable can be defined and initialized directly
+    static inline int s_ {5};
+
+/*since constexpr variables are implicitly inline they don't 
+need the inline keyword to be defined and initilized directly*/
+    static constexpr double s_height {1.95};
 };
