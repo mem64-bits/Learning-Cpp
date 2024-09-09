4d32217e0ae36bbc911ff3ae0e3e886645ee9855
shade64 <toryokodogbe270@gmail.com>
2024-09-09 21:35:46 +0000

    9 Sept 2024, 22:35

--- Object Oriented Programming/Classes/More on Classes/Friend Functions/usesOfFriendFunctions.cpp
+++ Object Oriented Programming/Classes/More on Classes/Friend Functions/usesOfFriendFunctions.cpp
@@ -4,7 +4,11 @@
 that other class or function is now a friend.
 
 This means the friend function has full access to public 
-and private and protected members of the class */
+and private and protected members of the class 
+
+A friend function is a function (member or non-member) that can 
+access the private and protected members of a class 
+as though it were a member of that class*/
 
 class Accumulator
 {
