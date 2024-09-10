eef22d06bebf636312625ab2223d97ca23143534
shade64 <toryokodogbe270@gmail.com>
2024-09-10 14:13:25 +0000

    10 Sept 2024, 15:13

---
+++ Object Oriented Programming/Classes/Questions/randomMonsterGen.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+
+
+
+class Monster
+{
+public:
+    enum Type
+{
+    dragon,
+    goblin,
+    orge,
+    orc,
+    skeleton,
+    troll,
+    vampire,
+    zombie,
+    maxMonsterTypes
+};
+
+Monster(Type type, std::string_view name, std::string_view roar, int hitPoints)
+: m_type{type}, m_name{name}
+, m_roar{roar}, m_hitPoints{hitPoints}
+{
+}
+
+constexpr std::string_view getStrFromType(const Type type) const
+{
+    switch(type)
+    {
+        case Monster::dragon: return "dragon";
+        case Monster::goblin: return "goblin";
+        case Monster::orge: return "orge";
+        case Monster::orc: return "orc";
+        
+        case Monster::skeleton: return "skeleton";
+        case Monster::troll: return "troll";
+        case Monster::vampire: return "vampire";
+        case Monster::zombie: return "zombie";
+
+        default: return "???";
+
+    }
+
+
+}
+
+void print() const 
+{   
+    if(m_hitPoints > 1)
+        std::cout<<m_name<<" the "<<getStrFromType(m_type)<<" has "
+        <<m_hitPoints<<" hit points and says "<<m_roar<<'\n';
+    else
+        std::cout<<m_name<<" the "<<getStrFromType(m_type)<<" is dead\n";
+}
+
+
+private:    
+    Type m_type{};
+    std::string m_name{};
+    std::string m_roar{};
+    int m_hitPoints{};
+};
+
+
+int main()
+{
+    Monster skeleton{ Monster::skeleton, "Bones", "*rattle*", 4 };
+	skeleton.print();
+
+	Monster vampire{ Monster::vampire, "Nibblez", "*hiss*", 0 };
+	vampire.print();
+
+    return 0;
+}
