f8441a366ab47284daf2619c7a22f20a5bd29098
shade64 <toryokodogbe270@gmail.com>
2024-09-10 18:14:19 +0000

    10 Sept 2024, 19:14

---
+++ Datatypes/Arrays/Usingstd::vector/usingStdVector.cpp
@@ -0,0 +1,13 @@
+#include <vector> // needed for std::vector
+#include <iostream>
+int main()
+{
+    // Uses list constructor
+    std::vector<int> prime_nums{2,3,5,7};
+
+// Uses CTAD C++17 to implicitly get type that vector contains
+    std::vector vowels{'a','e','i','o','u'};
+    for(int chars=0; chars < sizeof(vowels); ++chars)
+    std::cout<<chars<<' ';
+    return 0;
+}
