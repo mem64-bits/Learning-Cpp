80b87eddd7d4adcb9c4e7c588fa3d1121c6ba421
mem64-bits <toryokodogbe270@gmail.com>
2024-09-11 23:23:10 +0000

    12 Sept 2024, 00:23

---
+++ Datatypes/Arrays/Std::Vector/Passing std::vector/templateAutoVector.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+
+/*We can use auto in the template functions in 
+C++20 to do the same thing*/
+
+void passByRef(const auto& arr) // abbreviated function template
+{
+    std::cout << arr[0] << '\n';
+}
+
+int main()
+{
+    std::vector primes{ 2, 3, 5, 7, 11 };
+    passByRef(primes); // ok: compiler will instantiate passByRef(const std::vector<int>&)
+
+    std::vector dbl{ 1.1, 2.2, 3.3 };
+    passByRef(dbl);    // ok: compiler will instantiate passByRef(const std::vector<double>&)
+
+    return 0;
+}
