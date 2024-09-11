09bc6a3e28585a7def51f220b12495fe544c1d25
mem64-bits <toryokodogbe270@gmail.com>
2024-09-11 23:19:46 +0000

    12 Sept 2024, 00:19

--- Datatypes/Arrays/Std::Vector/Passing std::vector/templateVectors.cpp
+++ Datatypes/Arrays/Std::Vector/Passing std::vector/templateVectors.cpp
@@ -5,7 +5,7 @@
 we can use template functions*/
 
 template <typename T>
-void passByRef(std::vector<T>& arr)
+void passByRef(const std::vector<T>& arr)
 {
     std::cout<<arr[0]<<'\n';
 }
@@ -13,8 +13,8 @@ void passByRef(std::vector<T>& arr)
 int main()
 {
     std::vector primes{2, 3, 5, 7, 11}; // actually std::vector<int>
-    std::vector heights{1.74, 1.78, 1.82, 1.95, 2.10, 1.65}; // std::vector<double>
-    
     passByRef(primes); // template takes in std::vector<int> as an arguement
+
+    std::vector heights{1.74, 1.78, 1.82, 1.95, 2.10, 1.65}; // std::vector<double>
     passByRef(heights); // template takes in std::vector<doubles>
 }
