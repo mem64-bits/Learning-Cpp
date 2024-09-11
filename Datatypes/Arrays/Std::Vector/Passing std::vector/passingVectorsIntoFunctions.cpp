9ba300302245028d54fc0ce6f1a20c93cd1207b9
mem64-bits <toryokodogbe270@gmail.com>
2024-09-12 00:07:00 +0100

    std::vector

--- Datatypes/Arrays/Std::Vector/Passing std::vector/passingVectorsIntoFunctions.cpp
+++ Datatypes/Arrays/Std::Vector/Passing std::vector/passingVectorsIntoFunctions.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+/*we need to state the type of vector we are using*
+only can take in std::vectors holding ints*/
 
 void passByRef(std::vector<int>& arr)
 {
@@ -11,5 +13,13 @@ int main()
 {
     std::vector primes{2, 3, 5, 7, 9, 11};
     passByRef(primes);
+
+    std::vector heights{1.74, 1.78, 1.82, 1.95, 2.10, 1.65};
+// function can't take in std::vector<double> 
+    #if 0
+    passByRef(heights);
+    #endif
+
+
     return 0;    
 }
