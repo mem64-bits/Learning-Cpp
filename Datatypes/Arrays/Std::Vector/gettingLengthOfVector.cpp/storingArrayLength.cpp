e58fd97a508086fe456c9232dddeccfe0bebdc3a
shade64 <toryokodogbe270@gmail.com>
2024-09-11 19:39:40 +0000

    11 Sept 2024, 20:39

--- Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/storingArrayLength.cpp
+++ Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/storingArrayLength.cpp
@@ -9,7 +9,8 @@ Instead we can convert the length using static_cast*/
 int main()
 {
     std::vector primes {2, 3, 5, 7, 9, 11};
-    
+    int primes_length {static_cast<int>(std::size(primes))};
+    std::cout<<"length: "<<primes_length;
 
     return 0;
 }
