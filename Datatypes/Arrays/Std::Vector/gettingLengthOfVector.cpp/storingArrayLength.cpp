7fe3f7e7e823a60e8a3cc6c4cc56ef3782f0c824
shade64 <toryokodogbe270@gmail.com>
2024-09-11 19:17:53 +0000

    11 Sept 2024, 20:17

---
+++ Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/storingArrayLength.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*Because of soem weird quirks the length gotten 
+from std::size and .size is unsigned so we can store 
+the value in an int*/
+
+int main()
+{
+
+    return 0;
+}
+
