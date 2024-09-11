0071ad2d7cfdda5d4f6f44f517363af8d8c5bf02
shade64 <toryokodogbe270@gmail.com>
2024-09-11 19:26:04 +0000

    11 Sept 2024, 20:26

--- Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/storingArrayLength.cpp
+++ Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/storingArrayLength.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
+#include <vector>
 /*Because of soem weird quirks the length gotten 
 from std::size and .size is unsigned so we can't store 
 the value in an int*.
 
-*/
+Instead we can convert the length using static_cast*/
 
 int main()
 {
+    std::vector primes {2, 3, 5, 7, 9, 11};
+    
 
     return 0;
 }
