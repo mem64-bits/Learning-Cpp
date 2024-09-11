8b82fc8dd300a77a95abcda85ad44fd40339e82b
shade64 <toryokodogbe270@gmail.com>
2024-09-11 19:04:53 +0000

    11 Sept 2024, 20:04

--- Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/std::Size()AndSize().cpp
+++ Datatypes/Arrays/Std::Vector/gettingLengthOfVector.cpp/std::Size()AndSize().cpp
@@ -7,5 +7,7 @@ int main()
                         uses size type 
                         under the hood*/
     std::vector prime {2,3,5,7,11};
+    //prints length of array as an int
     std::cout<<"length: "<<prime.size();
 }
+
