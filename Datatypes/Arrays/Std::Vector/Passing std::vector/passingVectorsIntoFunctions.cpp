3e808d7dde757bd3bf8b8fe081d849f5deba2099
mem64-bits <toryokodogbe270@gmail.com>
2024-09-11 22:49:58 +0000

    11 Sept 2024, 23:49

--- Datatypes/Arrays/Std::Vector/Passing std::vector/passingVectorsIntoFunctions.cpp
+++ Datatypes/Arrays/Std::Vector/Passing std::vector/passingVectorsIntoFunctions.cpp
@@ -3,7 +3,13 @@
 
 
 void passByRef(std::vector<int>& arr)
+{
+    std::cout<<arr[0]<<'\n';
+}
+
 int main()
 {
-    
+    std::vector primes{2, 3, 5, 7, 9, 11};
+    passByRef(primes);
+    return 0;    
 }
