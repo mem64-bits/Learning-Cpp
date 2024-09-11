1203375ad9125fbd8a942eefd134f5c011d55e05
mem64-bits <toryokodogbe270@gmail.com>
2024-09-11 23:16:11 +0000

    12 Sept 2024, 00:16

--- Datatypes/Arrays/Std::Vector/Passing std::vector/templateVectors.cpp
+++ Datatypes/Arrays/Std::Vector/Passing std::vector/templateVectors.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 
+/*To use std::vector with multiple different types
+we can use template functions*/
+
 template <typename T>
 void passByRef(std::vector<T>& arr)
 {
     std::cout<<arr[0]<<'\n';
+}
+
+int main()
+{
+    std::vector primes{2, 3, 5, 7, 11}; // actually std::vector<int>
+    std::vector heights{1.74, 1.78, 1.82, 1.95, 2.10, 1.65}; // std::vector<double>
+    
+    passByRef(primes); // template takes in std::vector<int> as an arguement
+    passByRef(heights); // template takes in std::vector<doubles>
 }
