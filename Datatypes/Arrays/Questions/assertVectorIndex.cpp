3360aedc8d65e72986f7c0905ecac476cf4fc9fb
mem64-bits <toryokodogbe270@gmail.com>
2024-09-12 18:05:25 +0000

    12 Sept 2024, 19:05

--- Datatypes/Arrays/Questions/assertVectorIndex.cpp
+++ Datatypes/Arrays/Questions/assertVectorIndex.cpp
@@ -3,7 +3,7 @@
 
 void printElement(const auto& arr, int index)
 {
-    if(static_cast<int>(std::size(arr)) > index)
+    if(index > static_cast<int>(std::size(arr)))
         std::cout<<"Invalid index\n";
     else
         std::cout<<"The element has value "<<arr[index]<<'\n';
