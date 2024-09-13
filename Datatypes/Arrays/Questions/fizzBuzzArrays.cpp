42c372d6023131242a23ab59bbdb943bd83421e3
mem64-bits <toryokodogbe270@gmail.com>
2024-09-13 20:34:11 +0000

    13 Sept 2024, 21:34

---
+++ Datatypes/Arrays/Questions/fizzBuzzArrays.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+
+    
+
+
+
+int main()
+{
+    using namespace std::literals::string_view_literals;
+    std::vector<int> divisors {3, 5, 7, 11, 13, 17, 19};
+
+    std::vector<std::string_view> divisor_words {
+        "fizz"sv, "buzz"sv, "pop"sv, "bang"sv,
+        "jazz"sv, "pow"sv, "boom"sv
+    };
+
+    assert(divisors.size() == divisor_words.size() && "divisor array size does not match corresponding divisor words list") ;
+    
+
+
+}
