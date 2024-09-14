1bfae1d9fc98b9a183b984d6f13952605446e147
mem64-bits <toryokodogbe270@gmail.com>
2024-09-14 16:45:13 +0000

    14 Sept 2024, 17:45

--- Datatypes/Arrays/Std::Vector/Pitfalls of Signed and Unsigned/problemsWithUnsignedLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Pitfalls of Signed and Unsigned/problemsWithUnsignedLoops.cpp
@@ -5,9 +5,12 @@ template <typename T>
 void printReverse(const std::vector<T>& arr)
 {
     // index is unsigned
-    /* Since index is unsigned, using index >= 0 in the for loop
-    makes no sense as a unsigned num can't be 0 meaning the loop
-    would never end */
+
+    /* Since index is unsigned, using index >= 0 in the 
+    for loop makes no sense as a unsigned num can't be 
+    negative meaning the loop would never end 
+    
+    when */
     for(std::size_t index{arr.size()-1}; index >= 0; --index)
     {
         std::cout<<arr[index]<<' ';
@@ -20,6 +23,7 @@ int main()
     std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
 
     // undefined behaviour
+    //prints 9 1 2 8 3 7 6 4 then U.B
     printReverse(arr);
 
     return 0;
