b3b36d6622de04e5f1611cd8f82ce87d9f4d8ae6
mem64-bits <toryokodogbe270@gmail.com>
2024-09-14 17:48:02 +0000

    14 Sept 2024, 18:48

---
+++ Datatypes/Arrays/Std::Vector/Pitfalls of Signed and Unsigned/What Signed Type To Use/ptrdiff_t.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+
+
+/*
+std::size_t = unsigned
+std::ptrdiff_t  = signed 
+
+std::ptrdiff_t is essentially a special signed type 
+to be used with indexes
+
+std::ptrdiff_t has a weird name so it is often typedefed*/
+using Index = std::ptrdiff_t;
+
+
+int main()
+{
+
+    std::vector arr{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// Sample loop using index
+for (Index index{ 0 }; index < static_cast<Index>(arr.size()); ++index)
+{
+    if(arr[index] % 2 == 0)
+        std::cout<<arr[index]<<' ';
+}
+}
