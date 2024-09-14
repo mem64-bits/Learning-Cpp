7576455c7a13f88df48d0fbf6f9dfde2db5315f1
mem64-bits <toryokodogbe270@gmail.com>
2024-09-14 16:50:59 +0000

    14 Sept 2024, 17:50

---
+++ Datatypes/Arrays/Std::Vector/Pitfalls of Signed and Unsigned/problemsWithConvertingToSigned.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printReverse(const std::vector<T>& arr)
+{
+
+    /*Solves problem by converting signed,
+    but for loop is harder to read*/
+    
+    for (int index{ static_cast<int>(arr.size()) - 1}; index >= 0; --index) // index is signed
+    {
+        std::cout << arr[static_cast<std::size_t>(index)] << ' ';
+    }
+
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
+
+    printReverse(arr);
+
+    return 0;
+}
