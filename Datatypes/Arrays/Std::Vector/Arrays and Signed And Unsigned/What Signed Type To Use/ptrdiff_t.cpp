9bf38c8e0b6b4e6ecccc391d0af0d54e71904a31
mem64-bits <toryokodogbe270@gmail.com>
2024-09-14 17:59:11 +0000

    14 Sept 2024, 18:59

--- Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/What Signed Type To Use/ptrdiff_t.cpp
+++ Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/What Signed Type To Use/ptrdiff_t.cpp
@@ -25,10 +25,8 @@ void printEvenElements(std::vector<T>& arr)
 
 int main()
 {
-
     std::vector arr{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     // calls function using std::ptrdiff_t
     printEvenElements(arr);
     return 0;
-
 }
