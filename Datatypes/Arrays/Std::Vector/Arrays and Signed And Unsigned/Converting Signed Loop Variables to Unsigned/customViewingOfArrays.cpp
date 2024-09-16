c3e06ddef39cbf5c194e208dda73cb48b405b66f
mem64-bits <toryokodogbe270@gmail.com>
2024-09-16 20:13:38 +0000

    16 Sept 2024, 21:13

---
+++ Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/Converting Signed Loop Variables to Unsigned/customViewingOfArrays.cpp
@@ -0,0 +1,8 @@
+
+
+/*While we can’t modify the standard library 
+containers to accept a signed integral index,
+ we can create our own custom view class to “view” a 
+ standard library container class. And in doing so,
+  we can define our own interface to work however 
+  we want.*/
