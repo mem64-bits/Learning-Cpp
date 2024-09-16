23cecff151edadf0b360e9d6eb3c0a4403a94752
mem64-bits <toryokodogbe270@gmail.com>
2024-09-16 20:05:36 +0000

    16 Sept 2024, 21:05

--- Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/convertingSignedLoopVarToUsigned.cpp
+++ Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/convertingSignedLoopVarToUsigned.cpp
@@ -8,8 +8,10 @@ we should use a short named function as using static_cast
 over and over again makes code hard to read
 
 We use the functions std::is_integral and std::is_enum
-with static_assert to validate if we we converting the right type*/
+with static_assert to validate if we we converting 
+is an integral based type*/
 
+// alias makes type easier to understand
 using Index = std::ptrdiff_t;
 
 template <typename T>
@@ -17,4 +19,16 @@ constexpr std::size_t toUZ(T value)
 {
     static_assert(std::is_integral<T>() || std::is_enum<T>());
     return static_cast<std::size_t>(value);
+}
+
+int main()
+{
+    std::vector arr{ 9, 7, 5, 3, 1 };
+
+    //auto length { static_cast<Index>(arr.size()) };  // in C++20, prefer std::ssize()
+     auto length { static_cast<Index>(std::ssize(arr)) };
+    for (auto index{ length-1 }; index >= 0; --index)
+        std::cout << arr[toUZ(index)] << ' '; // use toUZ() to avoid sign conversion warning
+
+    return 0;
 }
