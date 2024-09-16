09602d8fa8a41e4c89d3f711d1f2625fce513779
mem64-bits <toryokodogbe270@gmail.com>
2024-09-16 20:02:14 +0000

    16 Sept 2024, 21:02

--- Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/convertingSignedLoopVarToUsigned.cpp
+++ Datatypes/Arrays/Std::Vector/Arrays and Signed And Unsigned/convertingSignedLoopVarToUsigned.cpp
@@ -2,6 +2,14 @@
 #include <type_traits> // for std::is_integral and std::is_enum
 #include <vector>
 
+
+/*When we convert from a signed to unsigned loop variable 
+we should use a short named function as using static_cast
+over and over again makes code hard to read
+
+We use the functions std::is_integral and std::is_enum
+with static_assert to validate if we we converting the right type*/
+
 using Index = std::ptrdiff_t;
 
 template <typename T>
