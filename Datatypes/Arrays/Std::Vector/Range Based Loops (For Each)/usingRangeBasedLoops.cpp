28d866207f5a2ef5535e26967f5a5facb14497f9
mem64-bits <toryokodogbe270@gmail.com>
2024-09-16 21:45:53 +0000

    16 Sept 2024, 22:45

--- Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/usingRangeBasedLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/usingRangeBasedLoops.cpp
@@ -11,16 +11,16 @@ int main()
 {
     std::vector nums{1, 2, 3 , 4, 5, 6, 7, 8, 9, 10};
 
+    std::cout<<"Even nums: [";
+
     for(int num:nums)
     {   
-        std::cout<<nums[num]<<' '<<'\n';
-
         if(nums[num] % 2 == 0)
-        {
-            std::cout<<"Even nums: [";
-            std::cout<<nums[num]<<' ';
-            std::cout<<']';
+        {    
+            std::cout<<nums[num]<<", ";
         }
     }
+    
+    std::cout<<']';
     return 0;
 }
