e28bf894ec5017e9e5a233e2809d6c3168aadf69
mem64-bits <toryokodogbe270@gmail.com>
2024-09-16 21:42:10 +0000

    16 Sept 2024, 22:42

--- Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/usingRangeBasedLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/usingRangeBasedLoops.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <vector>
 
+
+/*How range based loops work
+
+for (element_declaration : array_object)
+   statement
+*/
 int main()
 {
     std::vector nums{1, 2, 3 , 4, 5, 6, 7, 8, 9, 10};
 
-    for
+    for(int num:nums)
+    {   
+        std::cout<<nums[num]<<' '<<'\n';
+
+        if(nums[num] % 2 == 0)
+        {
+            std::cout<<"Even nums: [";
+            std::cout<<nums[num]<<' ';
+            std::cout<<']';
+        }
+    }
     return 0;
 }
