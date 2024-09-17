eb4491847afaaa42a8e39395c0e96906435d3e88
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 19:33:10 +0000

    17 Sept 2024, 20:33

--- Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/Reversing Range Based Loops/reverseRangeLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/Reversing Range Based Loops/reverseRangeLoops.cpp
@@ -2,3 +2,11 @@
 #include <ranges> // C++20
 #include <string>
 #include <vector>
+
+int main()
+{
+    std::vector<std::string_view> words {
+        "Alex", "Bobby", "Chad", "Dave"};
+        
+    return 0;
+}
