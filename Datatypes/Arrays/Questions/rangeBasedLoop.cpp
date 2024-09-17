a01b5cd6072caa1a72dd01032aa7e94c34da9e2c
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 20:26:40 +0000

    17 Sept 2024, 21:26

--- Datatypes/Arrays/Questions/rangeBasedLoop.cpp
+++ Datatypes/Arrays/Questions/rangeBasedLoop.cpp
@@ -9,6 +9,26 @@ int main()
         "Alex", "Betty", "Caroline", "Dave", 
         "Emily", "Fred", "Greg","Holly"};
 
-    
+    std::cout << "Enter a name: ";
+    std::string username{};
+    std::cin >> username;
+
+    bool found_name{false};
+
+    for(std::string_view name : names)
+    {
+        if(name == username )
+        {
+            found_name = true;
+            break;
+        }
+    }
+
+
+    if (found_name)
+        std::cout << username << " was found.\n";
+    else
+        std::cout << username << " was not found.\n";
+
     return 0;
 }
