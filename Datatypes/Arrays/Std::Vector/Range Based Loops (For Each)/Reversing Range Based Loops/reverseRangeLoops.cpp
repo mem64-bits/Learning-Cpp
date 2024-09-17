384e06afeef2a1097e6ce90dc55e4801e2a68f64
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 19:45:36 +0000

    17 Sept 2024, 20:45

--- Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/Reversing Range Based Loops/reverseRangeLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/Reversing Range Based Loops/reverseRangeLoops.cpp
@@ -3,6 +3,16 @@
 #include <string>
 #include <vector>
 
+/*
+    Range-based for loops only iterate in forwards order. in
+    by default in C++20 theres ways around this by using 
+    the function std::views::reverse
+    
+    It works by changing the way the array is viewed
+    so the loop is technicially moving forwards but 
+    has the effect of working the same way as iterating 
+    backwards
+*/
 
 int main()
 {
@@ -12,8 +22,9 @@ int main()
 
 //  creates a reverse view of the array words
     for(const auto& word : std::views::reverse(words))
-    {
+        std::cout<<word<<' ';
 
-    }
+    std::cout<<'\n';
+
     return 0;
 }
