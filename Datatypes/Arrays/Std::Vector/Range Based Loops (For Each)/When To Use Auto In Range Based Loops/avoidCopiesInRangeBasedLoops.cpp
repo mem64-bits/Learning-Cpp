cd08dacffe37c564667dc4c728328dd808aee636
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 18:58:45 +0000

    17 Sept 2024, 19:58

--- Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/When To Use Auto In Range Based Loops/avoidCopiesInRangeBasedLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/When To Use Auto In Range Based Loops/avoidCopiesInRangeBasedLoops.cpp
@@ -6,6 +6,7 @@ int main()
 {
     std::vector<std::string> words {"peter", "likes", "frozen", "yogurt"};
 
+// BAD :(
 // uses expensive copy of std::string
     for(auto word : words)
         std::cout<<word<<' ';
@@ -13,6 +14,17 @@ int main()
     std::cout<<'\n';
 
 
-    /*When iterating through an expensive type like std:string*/
+    /*When iterating through an expensive type like std:string
+    use const auto in the range based loop*/
 
+    for(const auto& word : words)
+        std::cout<<word<<' ';
+    std::cout<<'\n';
+
+    /* if we need to modify the type use a reference (auto&) */
+
+    for(auto& word : words)
+        std::cout<<word<<' ';
+    std::cout<<'\n';
+    return 0;
 }
