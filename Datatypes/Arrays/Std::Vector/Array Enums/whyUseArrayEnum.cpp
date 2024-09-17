1ad51dada56e8e8f0af0b0aae8bf9708a60bf228
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 20:56:46 +0000

    17 Sept 2024, 21:56

--- Datatypes/Arrays/Std::Vector/Array Enums/whyUseArrayEnum.cpp
+++ Datatypes/Arrays/Std::Vector/Array Enums/whyUseArrayEnum.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
+#include <vector>
 
 /*sometimes accessing the index of an array 
 can be hard to underd=stand and know what
 it represents*/
+
+int main()
+{
+    std::vector testScores { 78, 94, 66, 77, 14 };
+
+    testScores[2] = 76; // who does this represent?
+}
