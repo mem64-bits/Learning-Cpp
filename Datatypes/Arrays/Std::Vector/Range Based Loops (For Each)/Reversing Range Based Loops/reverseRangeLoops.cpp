f3df6d26b63d44020d7dfd728bd1839e8717b18b
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 19:39:15 +0000

    17 Sept 2024, 20:39

--- Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/Reversing Range Based Loops/reverseRangeLoops.cpp
+++ Datatypes/Arrays/Std::Vector/Range Based Loops (For Each)/Reversing Range Based Loops/reverseRangeLoops.cpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <vector>
 
+
 int main()
 {
+    // alphabetically sorted
     std::vector<std::string_view> words {
         "Alex", "Bobby", "Chad", "Dave"};
 
-//  creates a reverse view 
+//  creates a reverse view of the array words
     for(const auto& word : std::views::reverse(words))
     {
 
