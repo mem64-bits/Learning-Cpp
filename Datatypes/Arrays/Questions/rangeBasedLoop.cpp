522042dd2df54d088aceed4b9dcb908f208a0e1b
mem64-bits <toryokodogbe270@gmail.com>
2024-09-17 20:11:13 +0000

    17 Sept 2024, 21:11

--- Datatypes/Arrays/Questions/rangeBasedLoop.cpp
+++ Datatypes/Arrays/Questions/rangeBasedLoop.cpp
@@ -1,3 +1,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+template <typename T>
+bool findArrElem(const std::vector<T>& arr, auto name)
+{
+    for(const auto& element : arr)
+    {
+        if(element == name )
+            return true;
+    } 
+}
+
+std::string_view getName()
+{   
+    std::string name{};
+    std::cout<<"Enter a name: ";
+    std::cin>>name;
+    return name;
+}
+
+int main()
+{
+    std::vector<std::string> names {
+        "Alex", "Betty", "Caroline", "Dave", 
+        "Emily", "Fred", "Greg","Holly"};
+
+    std::string_view name{ getName() };
+
+    if(findArrElem(names, name))
+        std::cout<<'\n'<<name<<" was found\n";
+    
+    else
+        std::cout<<'\n'<<name<<" was found\n";
+    
+
+
+    return 0;
+}
