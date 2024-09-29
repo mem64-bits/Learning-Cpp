1c5982b82c0a9c91672eef9940c6d0f73fe4f9eb
mem64-bits <toryokodogbe270@gmail.com>
2024-09-29 12:09:06 +0000

    29 Sept 2024, 13:09

--- Challenges/Advanced Calculator/advancedCalC.cpp
+++ Challenges/Advanced Calculator/advancedCalC.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-template <typename T, typename U>
+template <typename T>
 class Calculator{
 private:
 
@@ -16,17 +16,42 @@ T m_operand{0};
 Operator operator{};
 
 public:
-    Calculator& add(T num){
-        m_operand += num;
-        return *this
+
+    Calculator(T operand)
+    :m_operand{operand}
+    {
     }
 
-    Calculator& sub(T num)
+    Calculator& add(T num){
+        m_operand += num;
+        return *this;
+    }
+
+    Calculator& sub(T num){
+        m_operand -= num;
+        return *this;
+    }
+
+    Calculator& mult(T num){
+        m_operand *= num;
+        return this;
+    }
+
+    Calculator& div(T num){
+        return *this;
+    }
+
+    void print() const{
+        std::cout<<m_operand;
+    }
 };
+
+
 
 int main()
 {
-
+    Calculator calc{1};
+    calc.add(5).sub(2).print();
     return 0;
 }
 
