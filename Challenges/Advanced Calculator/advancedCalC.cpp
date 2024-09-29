bee84c4cf2e1e32bc3c6f660266d50c07adc57d4
mem64-bits <toryokodogbe270@gmail.com>
2024-09-29 11:51:06 +0000

    29 Sept 2024, 12:51

--- Challenges/Advanced Calculator/advancedCalC.cpp
+++ Challenges/Advanced Calculator/advancedCalC.cpp
@@ -1,1 +1,27 @@
-#
+#include <iostream>
+#include <string>
+
+template <typename T, typename U>
+class Calculator{
+private:
+
+enum Operator{
+   ADD,
+   SUB,
+   MULT,
+   DIV,
+};
+
+T m_operand{0};
+Operator operator{};
+
+public:
+    
+};
+
+int main()
+{
+
+    return 0;
+}
+
