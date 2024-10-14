82fd8d66e1f108675b7de78acf597987d60b1034
mem64-bits <toryokodogbe270@gmail.com>
2024-10-14 11:56:15 +0000

    14 Oct 2024, 12:56

--- Datatypes/Arrays/Std::Vector/Array Enums/Array Enum Basics/arrayEnums.cpp
+++ Datatypes/Arrays/Std::Vector/Array Enums/Array Enum Basics/arrayEnums.cpp
@@ -18,10 +18,17 @@ namespace Students
 int main()
 {
     std::vector<int> testScores(Students::max_students); // Create a vector with 5 elements
-
+    
+    // puts value in testScores[2] under the hood 
     testScores[Students::stan] = 76; // we are now updating the test score belonging to stan
 
     std::cout << "The class has " << Students::max_students << " students\n";
+    
+    
+    std::cout<<testScores[Students::stan]<<'\n';
+
+    // Same as printing out testScores[Students::stan]
+    std::cout<<testScores[2]<<'\n';
 
     return 0;
 }
