02786fc12310714bb69afb217502c9518abdd45a
mem64-bits <toryokodogbe270@gmail.com>
2024-10-17 19:07:23 +0000

    17 Oct 2024, 20:07

---
+++ Datatypes/Arrays/Std::Vector/Array Enums/Avoiding Enum Index Conversion Errors/avoidingEnumConversions.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+namespace Students
+{
+    // we can avoid any sign conversion warnings
+    enum Names : unsigned int // explicitly specifies the underlying type is unsigned int
+    {
+        kenny, // 0
+        kyle, // 1
+        stan, // 2
+        butters, // 3
+        cartman, // 4
+        max_students // 5
+    };
+}
+
+int main()
+{
+    std::vector testScores { 78, 94, 66, 77, 14 };
+    Students::Names name { Students::stan }; // non-constexpr
+
+    testScores[name] = 76; // not a sign conversion since name is unsigned
+
+    return 0;
+}
