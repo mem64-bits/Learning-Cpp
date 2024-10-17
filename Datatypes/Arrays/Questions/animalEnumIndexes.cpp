09e086f6677760111b4cef20a5cbf027c1867b88
mem64-bits <toryokodogbe270@gmail.com>
2024-10-17 20:53:06 +0000

    17 Oct 2024, 21:53

---
+++ Datatypes/Arrays/Questions/animalEnumIndexes.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+namespace Animals{
+    enum Pet{
+        chicken,
+        dog,
+        cat,
+        elephant,
+        duck,
+        snake
+    };
+}
+
+int main(){
+
+    return 0;
+}
