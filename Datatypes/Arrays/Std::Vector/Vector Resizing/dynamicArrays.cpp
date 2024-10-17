90d3224e88c4d92ac03af4e48cf500e507f1a398
mem64-bits <toryokodogbe270@gmail.com>
2024-10-17 21:36:41 +0000

    17 Oct 2024, 22:36

--- Datatypes/Arrays/Std::Vector/Vector Resizing/dynamicArrays.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/dynamicArrays.cpp
@@ -2,9 +2,16 @@
 #include <vector>
 
 
-/*Unlike other array types in C++ std::vector's array
-size doesn't need to */
+/*Unlike other fixed array types like std::array and C-style arrays
+ in C++ std::vector's array size doesn't need to known at it's 
+ creation, and it can be changed. This is called a dynamic array*/
+
 int main(){
+    // std::vector can be resized by using the resize() function
+
+
+    std::vector v1{0,1,2};
+    std
 
     return 0;
 }
