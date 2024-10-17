bbc50da6d48e9fdf0ba018fe93fe3932f42e8b35
mem64-bits <toryokodogbe270@gmail.com>
2024-10-17 18:49:24 +0000

    17 Oct 2024, 19:49

---
+++ Datatypes/Arrays/Std::Vector/Array Enums/Avoiding Enum Index Conversion Errors/enumIndexConversionError.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+namespace Students{
+
+    enum Names{
+        kenny, // 0
+        kyle, // 1
+        stan, // 2
+        butters, // 3
+        cartman, // 4
+        max_students // 5
+    };
+
+}
+
+int main(){
+    std::vector testScores {78, 94, 66, 77}
+}
