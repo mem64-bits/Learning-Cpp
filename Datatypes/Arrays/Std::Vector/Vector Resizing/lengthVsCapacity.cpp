04ea04222d51dc7c27a8f3016275b32174adc01a
mem64-bits <toryokodogbe270@gmail.com>
2024-10-19 10:38:24 +0000

    19 Oct 2024, 11:38

---
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+
+//prints capacity and length of array
+void printCapLen(std::vector<int>& arr){
+    std::cout<<"Capacity: "<<arr.capacity()<<" Length:"
+    <<arr.size()<<'\n';
+}
+
+int main(){
+
+    return 0;
+}
