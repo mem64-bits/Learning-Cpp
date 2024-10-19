86ff11ff0afa77cf1f4e6189fbbbd1338d5d3bd4
mem64-bits <toryokodogbe270@gmail.com>
2024-10-19 11:08:47 +0000

    19 Oct 2024, 12:08

--- Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
@@ -1,28 +1,35 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
-void printCapLen(const std::vector<int>& v)
-{
-	std::cout << "Capacity: " << v.capacity() << " Length:"	<< v.size() << '\n';
+/*The capacity of an array is how much an array can hold
+The length of an array is how much items an array currently has*/
+
+// prints capacity and length of array
+void printCapLen(std::vector<int>& arr){
+    std::cout<<"Capacity: "<<arr.capacity()<<" Length:"
+    <<arr.size()<<'\n';
 }
 
-int main()
-{
-    std::vector v{ 0, 1, 2 }; // length is initially 3
+int main(){
+    std::vector even_nums{2,4,6}; // length is initially 3 
+    printCapLen(even_nums); // the length and capacity of the array is the same in this case
 
-    printCapLen(v);
+    // uses for each loop to print elements in array
+    for(auto i:even_nums)
+        std::cout<<i<<' ';
+    std::cout<<'\n';
+    
 
-    for (auto i : v)
-        std::cout << i << ' ';
-    std::cout << '\n';
 
-    v.resize(5); // resize to 5 elements
 
-    printCapLen(v);
+    even_nums.resize(5); // increases to capacity of vector to 5
+    even_nums.push_back(3);
+    printCapLen(even_nums);
 
-    for (auto i : v)
-        std::cout << i << ' ';
-    std::cout << '\n';
+    for(auto j:even_nums)
+        std::cout<<j<<' ';
+    std::cout<<'\n';
+    
 
     return 0;
 }
