0712f97a23ca2505e1a9434d08f46d325a97afb8
mem64-bits <toryokodogbe270@gmail.com>
2024-10-19 11:17:12 +0000

    19 Oct 2024, 12:17

--- Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
@@ -8,7 +8,7 @@ This means that the length and capacity of an array isn't always
 the same*/
 
 // prints capacity and length of array
-void printCapLen(std::vector<int>& arr){
+void printCapLen(const std::vector<int>& arr){
     std::cout<<"Capacity: "<<arr.capacity()<<" Length:"
     <<arr.size()<<'\n';
 }
