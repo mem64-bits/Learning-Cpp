7d17786e678e7d485aed5e7550387b7dd1c83bc0
mem64-bits <toryokodogbe270@gmail.com>
2024-10-19 10:55:19 +0000

    19 Oct 2024, 11:55

--- Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/lengthVsCapacity.cpp
@@ -10,6 +10,19 @@ void printCapLen(std::vector<int>& arr){
 
 int main(){
     std::vector even_nums{2,4,6}; // length is initially 3 
-    
+    printCapLen(even_nums); // the length and capacity of the array is the same in this case
+
+    // uses for each loop to print elements in array
+    for(auto i:even_nums){
+        std::cout<<i<<' ';
+    }
+
+    even_nums.resize(5); // increases to capacity of vector to 5
+    printCapLen(even_nums);
+
+    for(auto j:even_nums){
+        std::cout<<j<<' ';
+    }
+
     return 0;
 }
