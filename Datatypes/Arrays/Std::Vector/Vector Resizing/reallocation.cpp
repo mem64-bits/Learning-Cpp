1f8d4a8f21199edf8b9ebaca26101437f6df2197
mem64-bits <toryokodogbe270@gmail.com>
2024-10-19 11:46:22 +0000

    19 Oct 2024, 12:46

--- Datatypes/Arrays/Std::Vector/Vector Resizing/reallocation.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/reallocation.cpp
@@ -4,12 +4,16 @@
 int main(){
     std::vector<int>nums(1);
 
-    for(auto i : nums){
+    for(std::size_t i=0; i<std::size(nums); i++){
         for(std::size_t j=1; j<10; j++){
             nums.resize(j);
             nums.push_back(j);
             std::cout<<nums[j]<<'\n';
         }
     }
+
+    for(auto k : nums)
+        std::cout<<k<<' ';
+       
     return 0;
 }
