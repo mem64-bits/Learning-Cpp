ac93ad4a35abed1ba3af1c1ce55d52759a363ce1
mem64-bits <toryokodogbe270@gmail.com>
2024-10-20 14:37:43 +0000

    20 Oct 2024, 15:37

--- Datatypes/Arrays/Std::Vector/Stacks/whatIsAStack.cpp
+++ Datatypes/Arrays/Std::Vector/Stacks/whatIsAStack.cpp
@@ -3,7 +3,20 @@
 
 /*Stacks are a concept in programming, that represent a 
 container datatype, where elements can be inserted or removed
-in a Last in, first out (L.I.F.O) manner*/
-int main(){
+in a Last in, first out (L.I.F.O) manner
 
+There are to main concepts to manipulate stacks in
+programming 
+
+--------------------------------------------------
+Operation   Behaviour
+--------------------------------------------------
+Push|	    Put new element on top of stack
+Pop      Remove the top element from the stack
+
+*/
+
+int main(){
+    // a vector is an example of a stack datatype in C++
+    std::vector<int> stack{};
 }
