d2cbbe838f7b0a21c68451bf7b2d2bae5a070db7
mem64-bits <toryokodogbe270@gmail.com>
2024-10-20 13:37:59 +0000

    20 Oct 2024, 14:37

--- Datatypes/Arrays/Std::Vector/Vector Resizing/Shrinking Vector/vectorShrinking.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Resizing/Shrinking Vector/vectorShrinking.cpp
@@ -2,8 +2,23 @@
 #include <vector>
 
 
-/**/
-int main(){
+/*Resizing a vector to be  will increase the vector's length,
+the capacity of the vector if required,
 
+However decreasing the length of a vector only 
+decreases the length of the vector
+
+To help with this we can use the member function
+shrink_to_fit()*/
+
+void printCapLen(const std::vector<int>& v){
+    std::cout<<"Capacity: "<<v.capacity()<<" Length: "<<
+    v.size()<<'\n';
+}
+
+int main(){
+    std::vector<int> v(1000); // allocate room for 1000 elements
+    v.resize(0); // to resizes to 0 elements
+    v.shrink_to_fit();
     return 0;
 }
