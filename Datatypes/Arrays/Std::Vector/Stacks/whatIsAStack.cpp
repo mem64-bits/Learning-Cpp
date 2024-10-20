569ba7d300dda26c214f886b64249958686109e1
mem64-bits <toryokodogbe270@gmail.com>
2024-10-20 14:26:15 +0000

    20 Oct 2024, 15:26

---
+++ Datatypes/Arrays/Std::Vector/Stacks/whatIsAStack.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+/*Stacks are a concept in programming, that represent a 
+container datatype, where elements can be inserted or removed
+in a Last in, first out (L.I.F.O) manner*/
+int main(){
+
+}
