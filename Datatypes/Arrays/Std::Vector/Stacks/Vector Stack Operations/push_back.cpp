f1f8a19f08774de68137d2af5c46728e003ec46d
mem64-bits <toryokodogbe270@gmail.com>
2024-10-27 12:47:01 +0000

    27 Oct 2024, 12:47

---
+++ Datatypes/Arrays/Std::Vector/Stacks/Vector Stack Operations/push_back.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+int main(){
+
+    //creates a vector that can hold 10 elements
+    std::vector<int>nums(10);
+
+    // the push_back() member function puts in a new element at the
+
+    
+    
+    return 0;
+}
