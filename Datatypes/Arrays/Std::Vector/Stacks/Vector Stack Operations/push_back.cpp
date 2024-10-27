b6be8460ae3952b0a614002e9be69e78e9f8e6cb
mem64-bits <toryokodogbe270@gmail.com>
2024-10-27 12:55:01 +0000

    27 Oct 2024, 12:55

--- Datatypes/Arrays/Std::Vector/Stacks/Vector Stack Operations/push_back.cpp
+++ Datatypes/Arrays/Std::Vector/Stacks/Vector Stack Operations/push_back.cpp
@@ -1,13 +1,28 @@
 #include <vector>
 #include <iostream>
 
+
+void printStack(std::vector<int>& stack){
+    if(stack.empty())
+        std::cout<<"Empty";
+    
+    for(auto element : stack){
+        std::cout<<element<<' ';
+    }
+
+    
+}
 int main(){
 
-    //creates a vector that can hold 10 elements
-    std::vector<int>nums(10);
+    // creates a vector that can hold 10 elements
+    std::vector<int>stack(10);
 
-    // the push_back() member function puts in a new element at the
-
+    /*the push_back() member function puts in a new element
+    /at the top of the stack*/
+    
+    for(int i=1; i<stack.size()-1; i++){
+        stack.push_back(i);
+    }
     
     
     return 0;
