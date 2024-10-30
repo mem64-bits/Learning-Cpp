6a2cbbd2a1734a6a0bbf01cee5991f044ae16106
mem64-bits <toryokodogbe270@gmail.com>
2024-10-30 19:44:50 +0000

    30 Oct 2024, 19:44

--- Datatypes/Arrays/Std::Vector/Stacks/Vector Stack Operations/usingReserve.cpp
+++ Datatypes/Arrays/Std::Vector/Stacks/Vector Stack Operations/usingReserve.cpp
@@ -3,9 +3,22 @@
 
 /*
     we can use the reserve member function to change the 
-    capacity of the vectot but keep the length of the vector the same
+    capacity of the vector without changing the length of the vector
 */
+
+void printStack(const std::vector<int>& stack)
+{
+	if (stack.empty()) // if stack.size == 0
+		std::cout << "Empty";
+
+	for (auto element : stack)
+		std::cout << element << ' ';
+
+	// \t is a tab character, to help align the text
+	std::cout << "\tCapacity: " << stack.capacity() << "  Length " << stack.size() << "\n";
+}
+
 int main(){
-    
+
     return 0;
 }
