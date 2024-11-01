f03f78198b6011a12c6d5e39c809bbdc4a4e133b
mem64-bits <toryokodogbe270@gmail.com>
2024-11-01 13:49:28 +0000

    1 Nov 2024, 13:49

--- Datatypes/Arrays/Std::Vector/Vector Booleans/vectorBools.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Booleans/vectorBools.cpp
@@ -2,16 +2,15 @@
 #include <vector>
 
 int main(){
-    //vectors have a special function of making an array of booleans which 
-    std::vector<bool> bools{true,false,true,false,true};
-    for(int boolean : bools ){
-        if(boolean){
-            std::cout<<"true "; 
-        }
+    /*vectors have a special function of making an array of booleans which is useful in a number of ways
+     such as binary, but it doesn't have bit manipulation like std::bitset
+     
+     It also has a downside of requiring 40 bytes of overhead, and is diffren */
 
-        else{
-            std::cout<<"false ";
-        }
-    }
+    std::vector<bool> bools{true,false,true,false,true,false}; //101010
+
+    //prints out bools in intregal form
+    for(int boolean : bools )
+        std::cout<<boolean<<' '; 
     return 0;
 }
