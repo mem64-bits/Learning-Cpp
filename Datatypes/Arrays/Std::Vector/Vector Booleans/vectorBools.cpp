0aa8bbbd1f36a8d0a7241fea6ef6bdec8d0207f8
mem64-bits <toryokodogbe270@gmail.com>
2024-11-01 14:02:00 +0000

    1 Nov 2024, 14:02

--- Datatypes/Arrays/Std::Vector/Vector Booleans/vectorBools.cpp
+++ Datatypes/Arrays/Std::Vector/Vector Booleans/vectorBools.cpp
@@ -5,7 +5,16 @@ int main(){
     /*vectors have a special function of making an array of booleans which is useful in a number of ways
      such as binary, but it doesn't have bit manipulation like std::bitset
      
-     It also has a downside of requiring 40 bytes of overhead, and is diffren */
+     It also has a downside of requiring 40 bytes of overhead, and can work differently on
+     other platforms.
+
+    values do not need to be next o each other in memory and it isn't considered a container in C++
+    As a result its often not compatabile with code in standard lib C++
+
+    Avoid using std::vector<bool> 
+    use constexpr std::bitset, std::vector<char>, or 3rd party 
+    dynamic bitsets over std::vector<bool>.
+    */
 
     std::vector<bool> bools{true,false,true,false,true,false}; //101010
 
