0e13dd30cd57de3d7bceaf4b13e33aeb19b14e2b
mem64-bits <toryokodogbe270@gmail.com>
2024-11-02 15:30:24 +0000

    2 Nov 2024, 15:30

--- Datatypes/Arrays/Std::Array/ArrayCTAD/arrayDeduction.cpp
+++ Datatypes/Arrays/Std::Array/ArrayCTAD/arrayDeduction.cpp
@@ -1,7 +1,10 @@
 #include <array>
 
+/*std::array allows for CTAD, meaning we don't have to 
+explicitly include to type in the definition of the array*/
 
 int main(){
-    
+    std::array a {1.2,1.3,4.6,7.8,4.6}; // decudes type to std::array<double,5> 
+    std::array b {1,2,3,4,5}; // deduces type to std::array<int,5>
     return 0;
 }
