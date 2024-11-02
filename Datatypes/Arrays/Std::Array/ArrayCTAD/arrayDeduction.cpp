fc87c5d772f7187b5a2647b97fc555c871f8a35c
mem64-bits <toryokodogbe270@gmail.com>
2024-11-02 15:40:24 +0000

    2 Nov 2024, 15:40

--- Datatypes/Arrays/Std::Array/ArrayCTAD/arrayDeduction.cpp
+++ Datatypes/Arrays/Std::Array/ArrayCTAD/arrayDeduction.cpp
@@ -1,10 +1,13 @@
 #include <array>
 
-/*std::array allows for CTAD, meaning we don't have to 
+/*std::array allows for CTAD (C++17), meaning we don't have to 
 explicitly include to type in the definition of the array*/
 
 int main(){
     std::array a {1.2,1.3,4.6,7.8,4.6}; // decudes type to std::array<double,5> 
     std::array b {1,2,3,4,5}; // deduces type to std::array<int,5>
+
+    // However it doesn't support partial deduction e.g
+    // std::array<5> c {2,4,6,8,10};
     return 0;
 }
